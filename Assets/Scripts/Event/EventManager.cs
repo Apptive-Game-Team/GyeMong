ee8b2b5343019628570506fc9875676dd4ec6be4
mandu <jys0972@gmail.com>
2024-11-01 14:09:15 +0900

    feat(EventManager.cs) : HurtEffect 관련 로직 구현

--- Assets/Scripts/Event/EventManager.cs
+++ Assets/Scripts/Event/EventManager.cs
@@ -1,20 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+/// <summary>
+/// Every Event Management class
+/// </summary>
 public class EventManager : SingletonObject<EventManager>
 {
 
     private CameraController cameraController;
+    private RawImage hurtEffect;
 
     public void SetCameraController(CameraController cameraController)
     {
         this.cameraController = cameraController;
     }
 
-    
+    /// <summary>
+    /// set HurtEffect transparency
+    /// </summary>
+    /// <param name="amount">0(none)~1(fill) alpha value</param>
+    public void HurtEffect(float amount)
+    {
+        Color color = hurtEffect.color;
+        color.a = amount;
+        hurtEffect.color = color;
+    }
+
+    /// <summary>
+    /// vibrate camera
+    /// </summary>
     public void ShakeCamera()
     {
         cameraController.ShakeCamera();
+    }
+
+    private void CachingImages()
+    {
+        hurtEffect = transform.Find("HurtEffect").GetComponent<RawImage>();
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        CachingImages();
     }
 }
