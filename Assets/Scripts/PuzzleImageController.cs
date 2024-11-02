d31927eecd6f747a119bad37d2b3a48e31a320a2
191012 <kimhj191012@naver.com>
2024-11-02 14:02:01 +0900

    feat(TileMap)
    퍼즐 근처 타일맵 깔기

--- Assets/Scripts/PuzzleImageController.cs
+++ Assets/Scripts/PuzzleImageController.cs
@@ -12,6 +12,11 @@ public class PuzzleImageController : MonoBehaviour
         SetUpInitialRotation();
     }
 
+    void Update()
+    {
+        
+    }
+
     void SetUpImage()
     {
         foreach (Transform image in transform)
@@ -23,10 +28,26 @@ public class PuzzleImageController : MonoBehaviour
     void SetUpInitialRotation()
     {
         float[] rotations = { 0f, 90f, 180f, 270f };
+
+        do
+        {
+            foreach (GameObject image in puzzleImages)
+            {
+                float randomRotation = rotations[Random.Range(0, rotations.Length)];
+                image.transform.rotation = Quaternion.Euler(0f, 0f, randomRotation);
+            }
+        } while (CheckClear());
+    }
+
+    bool CheckClear()
+    {
         foreach (GameObject image in puzzleImages)
         {
-            float randomRotation = rotations[Random.Range(0, rotations.Length)];
-            image.transform.rotation = Quaternion.Euler(0f, 0f, randomRotation);
+            if (Mathf.Abs(image.transform.eulerAngles.z % 360) != 0f)
+            {
+                return false;
+            }
         }
+        return true;
     }
 }
