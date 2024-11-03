4b0d084e0d5acc8d602faf70befd537531c38b14
hwanginseop <163392234+hwanginseop@users.noreply.github.com>
2024-11-03 16:26:41 +0900

    refact(DetectPlayer())
    트리거 조건 수정

--- Assets/Scripts/BossRoom/Entrance.cs
+++ Assets/Scripts/BossRoom/Entrance.cs
@@ -6,7 +6,7 @@ public class Entrance : MonoBehaviour
 {
     public MidBoss midBoss;
 
-    public void OnTriggerEnter2D(Collider2D other)
+    public void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
