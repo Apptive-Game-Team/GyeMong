122652a8f7a9edff5744d42e54817f4e3d5a44b9
mandu <jys0972@gmail.com>
2024-11-09 19:17:39 +0900

    feat(SoundManager.cs) : BGM Object 참조 설정

--- Assets/Scripts/Event/Sound/SoundManager.cs
+++ Assets/Scripts/Event/Sound/SoundManager.cs
@@ -8,12 +8,15 @@ public enum SoundType
     UI,
     EFFECT,
     FRIENDLY,
-    ENEMY
+    ENEMY,
+    BGM
 }
 
 public class SoundManager : SingletonObject<SoundManager>
 {
     private const float DEFAULT_VOLUME = 1;
+
+    private SoundObject bgmSoundObject;
 
     public SoundSourceList soundSourceList;
     private Dictionary<SoundType, float> volumes = new Dictionary<SoundType, float>();
@@ -23,6 +26,16 @@ public class SoundManager : SingletonObject<SoundManager>
         foreach (SoundType type in Enum.GetValues(typeof(SoundType))){
             volumes[type] = DEFAULT_VOLUME;
         }
+    }
+
+    public SoundObject GetBgmObject()
+    {
+        if (bgmSoundObject == null)
+        {
+            bgmSoundObject = gameObject.AddComponent<SoundObject>();
+        }
+        
+        return bgmSoundObject;
     }
 
     protected override void Awake()
