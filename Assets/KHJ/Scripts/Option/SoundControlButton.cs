fa2dd1f122fac8337c588b794a8b61ba8d71faa8
191012 <kimhj191012@naver.com>
2024-11-12 11:37:28 +0900

    fix(SoundCountrolButton.cs) : 사운드 설정 창을 닫으면 옵션창이 뜨도록 변경

--- Assets/KHJ/Scripts/Option/SoundControlButton.cs
+++ Assets/KHJ/Scripts/Option/SoundControlButton.cs
@@ -5,18 +5,9 @@ using UnityEngine.UI;
 
 public class SoundControlButton : MonoBehaviour
 {
-    private Image SoundControlImage;
-
-    private void Start()
-    {
-        Transform SoundControllImageTransform = transform.Find("SoundControlImage");
-        SoundControlImage = SoundControllImageTransform.GetComponent<Image>();
-        SoundControlImage.gameObject.SetActive(false);
-    }
-
     public void OnClickButton()
     {
         Option.Instance.OpenOrCloseOption();
-        SoundControlImage.gameObject.SetActive(true);
+        SoundControlImage.Instance.OpenSoundControlImage();
     }
 }
