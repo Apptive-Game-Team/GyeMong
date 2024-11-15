97f206ef43f28945e299753ebbdc5fa02d2638f2
mandu <jys0972@gmail.com>
2024-11-15 12:04:09 +0900

    feat(EventObject.cs) : ToggeableCondition static하게 모두 관리하는 로직 추가

--- Assets/Scripts/Event/EventObject.cs
+++ Assets/Scripts/Event/EventObject.cs
@@ -20,14 +20,44 @@ public class EventObject : InteractableObject
     [SerializeField]
     private int triggerLimitCounter = -1;
 
-    [SerializeField]
     [SerializeReference]
-    public List<Event> eventSequence = new List<Event>();
+    private List<Event> eventSequence = new List<Event>();
 
     private Coroutine eventLoop = null;
 
+    public static Dictionary<string, List<ToggeableCondition>> toggleableConditions = new();
+
+    private List<ToggeableCondition> FindToggleableConditions()
+    {
+        List<ToggeableCondition> result = new List<ToggeableCondition>();
+        foreach (Event @event in eventSequence)
+        {
+            List<ToggeableCondition> temp = @event.FindToggleableConditions();
+            if (temp != null)
+            {
+                result.AddRange(temp);
+            }
+        }
+        return result;
+    }
+
+    private void InitializeToggleableConditions()
+    {
+        List<ToggeableCondition> conditions =  FindToggleableConditions();
+        foreach (ToggeableCondition condition in conditions)
+        {
+            string tag = condition.GetTag();
+            if (!toggleableConditions.ContainsKey(tag))
+            {
+                toggleableConditions.Add(tag, new List<ToggeableCondition>());
+            }
+            toggleableConditions[tag].Add(condition);
+        }
+    }
+
     private void Start()
     {
+        InitializeToggleableConditions();
         if (trigger == EventTrigger.OnAwake && triggerLimitCounter != 0)
         {
             TriggerEvent();
