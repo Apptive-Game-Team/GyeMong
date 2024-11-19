b0d00e879ef0f79275b363feea6911b221ce7735
191012 <kimhj191012@naver.com>
2024-11-19 23:44:41 +0900

    feat(DataManager.cs) : 데이터 Save/Load를 관리하는 매니저 생성

---
+++ Assets/Scripts/Data/DataManager.cs
@@ -0,0 +1,36 @@
+using System.IO;
+using UnityEngine;
+
+public class DataManager : SingletonObject<DataManager>
+{
+    private readonly string savePath = Application.dataPath + "/Database";
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    // 특정 구역 저장
+    public void SaveSection<T>(T sectionData, string fileName) where T : class
+    {
+        string json = JsonUtility.ToJson(sectionData, true); // JSON으로 직렬화
+        File.WriteAllText(Path.Combine(savePath, fileName + ".json"), json); // 파일 저장
+        Debug.Log($"{fileName} saved at {savePath}");
+    }
+
+    // 특정 구역 불러오기
+    public T LoadSection<T>(string fileName) where T : class, new()
+    {
+        string filePath = Path.Combine(savePath, fileName + ".json");
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath); // JSON 파일 읽기
+            return JsonUtility.FromJson<T>(json); // JSON에서 객체로 역직렬화
+        }
+        else
+        {
+            Debug.LogWarning($"{fileName} not found, returning new instance.");
+            return new T(); // 파일이 없으면 새 인스턴스 반환
+        }
+    }
+}
