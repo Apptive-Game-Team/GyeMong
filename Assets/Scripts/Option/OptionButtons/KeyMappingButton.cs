66ba0ce18770569bdeab743c106d43fbd9390317
191012 <kimhj191012@naver.com>
2024-11-19 23:45:18 +0900

    refact(KeyMapping) : KeyMapping에 데이터 Save/Load 기능 적용

--- Assets/Scripts/Option/OptionButtons/KeyMappingButton.cs
+++ Assets/Scripts/Option/OptionButtons/KeyMappingButton.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class KeyMappingButton : MonoBehaviour
 {
     public void OnClickButton()
     {
         OptionUI.Instance.isOptionUITop = false;
+        KeyMappingData keyMappingData = DataManager.Instance.LoadSection<KeyMappingData>("KeyMappingData");
+        for (int i = 0;i < keyMappingData.keyBindings.Count;i++) 
+        {
+            KeyMappingEntry entry = keyMappingData.keyBindings[i];
+
+            ActionCode actionCode = (ActionCode)Enum.Parse(typeof(ActionCode), entry.actionCode);
+            KeyCode keyCode = (KeyCode)Enum.Parse(typeof(KeyCode), entry.keyCode);
+
+            InputManager.Instance.SetKey(actionCode, keyCode);
+        }
+        KeyButtonTexts.Instance.UpdateKeyText();
         KeyMappingUI.Instance.OpenKeyMappingUI();
     }
 }
