54967fd3a9e8195b97e91389d95f64948450f150
mandu <jys0972@gmail.com>
2024-11-20 20:57:05 +0900

    feat(Events.cs) : Event execute에 컨텍스트를 받을 수 있도록 수정

--- Assets/Scripts/Event/Events.cs
+++ Assets/Scripts/Event/Events.cs
@@ -8,7 +8,7 @@ using UnityEditor.PackageManager;
 [Serializable]
 public abstract class Event
 {
-    public abstract IEnumerator execute();
+    public abstract IEnumerator execute(EventObject eventObject = null);
     public virtual List<ToggeableCondition> FindToggleableConditions()
     {
         return null;
@@ -19,7 +19,7 @@ public abstract class Event
 public class HurtEffectEvent : Event
 {
     public float amount;
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         return EffectManager.Instance.HurtEffect(amount);
     }
@@ -29,7 +29,7 @@ public class HurtEffectEvent : Event
 public class ShakeCameraEvent : Event
 {
     public float time;
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         return EffectManager.Instance.ShakeCamera(time);
     }
@@ -39,7 +39,7 @@ public class ShakeCameraEvent : Event
 public class WarpPortalEvent : Event
 {
     public PortalID portalID;
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         return PortalManager.Instance.TransitScene(portalID);
     }
@@ -49,7 +49,7 @@ public class WarpPortalEvent : Event
 public class DelayEvent : Event
 {
     public float delayTime;
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         yield return new WaitForSeconds(delayTime);
     }
@@ -58,7 +58,7 @@ public class DelayEvent : Event
 [Serializable]
 public class FadeInEvent : Event
 {
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         return EffectManager.Instance.FadeIn();
     }
@@ -67,7 +67,7 @@ public class FadeInEvent : Event
 [Serializable]
 public class FadeOutEvent : Event
 {
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         return EffectManager.Instance.FadeOut();
     }
@@ -91,7 +91,7 @@ public class SoundEvent : Event
     [SerializeField]
     private string soundName = null;
 
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         if (playOrStop == PlayOrStop.STOP)
         {
@@ -112,7 +112,7 @@ public class BGMEvent : Event
 {
     [SerializeField]
     private string bgmName;
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         SoundObject soundObject = SoundManager.Instance.GetBgmObject();
         soundObject.SetSoundSourceByName(bgmName);
@@ -125,7 +125,7 @@ public class StopEvent : Event
 {
     [SerializeField]
     private EventObject targetObject;
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         targetObject.KillEvent();
         yield return null;
@@ -138,7 +138,7 @@ public class NestedEventEvent : Event
     [SerializeReference]
     private List<Event> events;
 
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         foreach (Event @event in events)
         {
@@ -172,7 +172,7 @@ public class ConditionalBranchEvent : Event
     [SerializeReference]
     private Event eventInFalse;
 
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         if (condition.Check())
         {
@@ -221,7 +221,7 @@ public class ConditionalLoopEvent : Event
     [SerializeReference]
     private Event loopBodyevent;
 
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         while (condition.Check())
         {
@@ -253,7 +253,7 @@ public class ToggleConditionEvent : Event
     [SerializeReference]
     private bool condition;
 
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         List<ToggeableCondition> conditions = EventObject.toggleableConditions[tag];
         if (conditions == null)
@@ -284,7 +284,7 @@ public class MovePositionEvent : Event
     [SerializeField]
     private Vector3 targetPosition;
 
-    public override IEnumerator execute()
+    public override IEnumerator execute(EventObject eventObject = null)
     {
         @gameObject.transform.position = targetPosition;
         yield return null;
