8e260f15833a893017297872ff65e2cf5be40ce1
191012 <kimhj191012@naver.com>
2024-11-21 16:42:11 +0900

    feat(Light System) : light 배치 및 설정
    미로 입구를 넘어가면 화면에 fade out/in 효과를 주는 light system 구현

---
+++ Assets/Scripts/Puzzle1/MazeDarkness.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class MazeDarkness : MonoBehaviour
+{
+    private Light2D globalLight;
+    private GameObject playerLight;
+    private GameObject player;
+    private bool isInMaze = false;
+    private const float changeRatio = 0.01f;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        globalLight = GameObject.Find("GlobalLight2D").GetComponent<Light2D>();
+        playerLight = player.transform.GetChild(0).GetComponent<Light2D>().gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            bool previousState = isInMaze;
+            isInMaze = player.transform.position.y > transform.position.y;
+
+            if (previousState != isInMaze)
+            {
+                StopAllCoroutines();
+                StartCoroutine(ChangeIntensity(isInMaze));
+            }
+        }
+    }
+
+    private IEnumerator ChangeIntensity(bool isInMaze)
+    {
+        float targetIntensity = isInMaze ? 0f : 1f;
+        float Ratio = 0f;
+        
+        while (Mathf.Abs(globalLight.intensity - targetIntensity) > 0.01f)
+        {
+            Ratio += Time.deltaTime;
+            globalLight.intensity = Mathf.Lerp(globalLight.intensity, targetIntensity, Ratio);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        playerLight.SetActive(isInMaze);
+
+        yield return null;
+    }
+}
