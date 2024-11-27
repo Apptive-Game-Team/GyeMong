0ecfd6fcf1f25498e726d12af616a379fcd1963d
191012 <kimhj191012@naver.com>
2024-11-27 14:13:21 +0900

    feat(ContinueButton.cs) : 타이틀에 계속하기 버튼 추가
    저장된 데이터가 있으면 버튼이 보이고, 없으면 안보임

---
+++ Assets/Scripts/TItle/ContinueButton.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueButton : MonoBehaviour
+{
+    private PlayerData playerData;
+    private void Start() 
+    {
+        playerData = DataManager.Instance.LoadSection<PlayerData>("PlayerData");
+        if (playerData.isFirst) gameObject.SetActive(!playerData.isFirst);
+    }
+
+    public void ContinueGame()
+    {
+        string sceneName = playerData.sceneName;
+        SceneManager.LoadScene(sceneName);
+    }
+}
