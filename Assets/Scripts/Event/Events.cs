227cc6227c748f6312443061d765e5b58ace1e32
mandu <jys0972@gmail.com>
2024-11-29 12:38:41 +0900

    refact(Events.cs)

--- Assets/Scripts/Event/Events.cs
+++ Assets/Scripts/Event/Events.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using UnityEditor.PackageManager;
-
+using Unity.VisualScripting;
 
 [Serializable]
 public abstract class Event
