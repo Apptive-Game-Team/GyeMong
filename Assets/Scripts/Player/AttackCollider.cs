ef968cf8d0e274328378f55bfcef51f3b33aa1c2
mandu <jys0972@gmail.com>
2024-11-29 15:27:47 +0900

    feat(AttackCollider.cs) : ParticleSystem과 연결

--- Assets/Scripts/Player/AttackCollider.cs
+++ Assets/Scripts/Player/AttackCollider.cs
@@ -7,8 +7,14 @@ public class AttackCollider : MonoBehaviour
 {
     public float attackDamage;
     private PlayerSoundController _soundController;
+    private EventObject _eventObject;
+    private ParticleSystem _particleSystem;
+    private ParticleSystem.ShapeModule _shape;
     private void Start()
     {
+        _particleSystem = GetComponentInChildren<ParticleSystem>();
+        _shape = _particleSystem.shape;//.GetComponent<ParticleSystem.ShapeModule>();
+        _eventObject = GetComponent<EventObject>();
         var player = PlayerCharacter.Instance;
         attackDamage = player.attackPower;
     }
@@ -24,15 +30,21 @@ public class AttackCollider : MonoBehaviour
         var creature = collision.GetComponent<Creature>();
         if (creature != null)
         {
+            SetParticleSystemTexture(collision);
+
+            _eventObject.TriggerEvent();
             _soundController.Trigger(PlayerSoundType.SWORD_ATTACK);
             creature.TakeDamage(attackDamage);
             Debug.Log("�ѱ��б�");
-            Destroy(gameObject);
+            // Destroy(gameObject);
         } else
         {
             IAttackable[] attackableObjects = collision.GetComponents<IAttackable>();
             if (attackableObjects.Length != 0)
             {
+                SetParticleSystemTexture(collision);
+                
+                _eventObject.TriggerEvent();
                 _soundController.Trigger(PlayerSoundType.SWORD_ATTACK);
                 foreach (IAttackable @object in attackableObjects)
                 {
@@ -40,5 +52,21 @@ public class AttackCollider : MonoBehaviour
                 }
             }
         }
+        
+    }
+
+    private void SetParticleSystemTexture(Collider2D collision)
+    {
+        Sprite sprite;
+        try
+        {
+            sprite = collision.GetComponent<SpriteRenderer>().sprite;
+            
+        }
+        catch (MissingComponentException)
+        {
+            sprite = collision.GetComponentInChildren<SpriteRenderer>().sprite;
+        }
+        _shape.texture = sprite.texture;
     }
 }
