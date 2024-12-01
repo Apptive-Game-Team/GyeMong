8e05258746619d6a53b7376a382706e66b29e8cd
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2024-12-01 15:23:04 +0900

    feat(Directing) : 연출
    루틴 :
    1. 화면 암전에서 밝아지기
    2. 카메라 이동
    3. 대화 스크립트

---
+++ Assets/Scripts/Event/SceneEvent/SceneEvent.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneEvent : MonoBehaviour
+{
+    public Image fadeImage;
+    public Camera mainCamera;
+    public Transform cameraTargetPosition;
+    public Vector3 originalCameraPosition;
+
+    private float waitCameraMoveDuration = 3.0f;
+    public CameraController cameraController;
+    public EventObject chatEventObject;
+
+    private void Start()
+    {    
+        originalCameraPosition = mainCamera.transform.position;
+        cameraController = mainCamera.GetComponent<CameraController>();
+        StartCoroutine(SceneTransition());
+    }
+
+    private IEnumerator SceneTransition()
+    {
+        yield return StartCoroutine(FadeIn());
+
+        yield return StartCoroutine(MoveCamera(cameraTargetPosition.position, 3f));
+
+        StopCameraFollow();
+
+        Debug.Log("Wait..");
+        yield return new WaitForSeconds(waitCameraMoveDuration);
+        Debug.Log("Finish");
+
+        yield return StartCoroutine(MoveCamera(originalCameraPosition, 2f));
+
+        StopCameraFollow();
+
+        Debug.Log("Wait..");
+        yield return new WaitForSeconds(waitCameraMoveDuration);
+        Debug.Log("Finish");
+
+        ResumeCameraFollow();
+
+        chatEventObject.TriggerEvent();
+    }
+
+    private IEnumerator FadeIn()
+    {
+        Debug.Log("FadeIn Start");
+
+        float duration = 3f;
+        float elapsedTime = 0f;
+
+        Color color = fadeImage.color;
+        color.a = 1f;
+        fadeImage.color = color;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Lerp(1f, 0f, elapsedTime / duration);
+            fadeImage.color = color;
+            yield return null;
+        }
+        Debug.Log("FadeIn End");
+
+        color.a = 0f;
+        fadeImage.color = color;
+    }
+
+    private IEnumerator MoveCamera(Vector3 targetPos, float duration)
+    {
+        Debug.Log("Camera Move Start");
+
+        float elapsedTime = 0f;
+        Vector3 startPos = mainCamera.transform.position;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            mainCamera.transform.position = Vector3.Lerp(startPos, targetPos, elapsedTime / duration);
+            yield return null;
+        }
+
+        mainCamera.transform.position = targetPos;
+
+        Debug.Log("Camera Move End");
+    }
+
+    private void StopCameraFollow()
+    {
+        cameraController.enabled = false;
+    }
+
+    private void ResumeCameraFollow()
+    {
+        cameraController.enabled = true;
+    }
+}
