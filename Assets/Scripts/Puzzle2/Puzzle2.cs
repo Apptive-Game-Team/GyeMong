43baf1bfcf9377dd597950fff08dfdd919334fac
mandu <jys0972@gmail.com>
2024-12-20 13:15:18 +0900

    feat(Puzzle2.cs) : Path를 벗어났을 때의 이벤트를 발생 시켰습니다.

--- Assets/Scripts/Puzzle2/Puzzle2.cs
+++ Assets/Scripts/Puzzle2/Puzzle2.cs
@@ -1,23 +1,35 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using playerCharacter;
 
-public class Puzzle2 : MonoBehaviour
+public class Puzzle2 : MonoBehaviour, IEventTriggerable
 {
     private Collider2D[] paths;
-    List<GameObject> roots;
+    private Transform[] roots;
     Tilemap fogTilemap;
     PlayerCharacter player;
+    Collider2D playerCollider;
     List<Collider2D> pathList;
+    private EventObject eventObject;
+    private EventStatus<int> rootNum;
 
     const int visionRange = 2;
     int curRootNum = 0;
 
     private void Start()
     {
+        rootNum = GetComponent<IntEventStatus>();
+        eventObject = GetComponent<EventObject>();
         player = PlayerCharacter.Instance;
+        playerCollider = player.GetComponent<Collider2D>();
+        GameObject pRoots = transform.Find("Roots").gameObject;
+        roots = transform.Find("Roots").GetComponentsInChildren<Transform>()
+            .Where(component => component.gameObject != pRoots && component.transform.parent == pRoots.transform)
+            .ToArray();;
+        Array.Sort(roots, (a, b) => a.GetSiblingIndex().CompareTo(b.GetSiblingIndex()));
         paths = transform.Find("Path").GetComponentsInChildren<Collider2D>();
         fogTilemap = GameObject.Find("fogTilemap").GetComponent<Tilemap>();
     }
@@ -25,6 +37,11 @@ public class Puzzle2 : MonoBehaviour
     private void Update()
     {
         UpdateFog();
+
+        if (!CheckOnPath())
+        {
+            eventObject.Trigger();
+        }
     }
 
     void UpdateFog()
@@ -39,10 +56,29 @@ public class Puzzle2 : MonoBehaviour
             }
         }
     }
+
+    bool CheckOnPath()
+    {
+        bool isOn = false;
+        foreach (Collider2D collider in paths)
+        {
+            isOn |= collider.IsTouching(playerCollider);
+        }
+        return isOn;
+    }
     
     void UpdateResetTime()
     {
-
+        
     }
 
+    private void MovePlayerPosition()
+    {
+        print(rootNum.GetStatus());
+        player.transform.position = roots[rootNum.GetStatus()].position;
+    }
+    public void Trigger()
+    {
+        MovePlayerPosition();
+    }
 }
