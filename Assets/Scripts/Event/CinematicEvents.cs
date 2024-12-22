60fd49dd0f74faa697db1040c6e1decfa8b3c10d
mandu <jys0972@gmail.com>
2024-12-22 21:32:05 +0900

    feat(CinematicEvents.cs) : Image 띄우고 치우는 Event 구현

--- Assets/Scripts/Event/CinematicEvents.cs
+++ Assets/Scripts/Event/CinematicEvents.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using playerCharacter;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public abstract class CinematicEvent : Event { }
 
@@ -118,6 +120,48 @@ public class ControlShaderEvent : CinematicEvent
         return null;
     }
 }
+
+public abstract class ImageEvent : CinematicEvent
+{
+    private Image _image;
+    protected Image Image
+    {
+        get
+        {
+            if (_image == null)
+            {
+                _image = EffectManager.Instance.transform.Find("EventImage").GetComponent<Image>();
+            }
+            return _image;
+        }
+    }
+}
+
+public class ShowImageEvent : ImageEvent
+{
+    [SerializeField] private Sprite sprite;
+    
+    public override IEnumerator Execute(EventObject eventObject = null)
+    {
+        Color color = Image.color;
+        color.a = 1;
+        Image.color = color;
+        Image.sprite = sprite;
+        return null;
+    }
+}
+public class ClearImageEvent : ImageEvent
+{
+    public override IEnumerator Execute(EventObject eventObject = null)
+    {
+        Color color = Image.color;
+        color.a = 0;
+        Image.color = color;
+        Image.sprite = null;
+        return null;
+    }
+}
+
 public abstract class AnimationControllEvent : CinematicEvent
 {
     [SerializeField] protected Animator _animator;
