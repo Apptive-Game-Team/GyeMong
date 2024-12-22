8ef4e60d21125172984357e60bb850e5924446fc
mandu <jys0972@gmail.com>
2024-12-22 21:16:28 +0900

    feat(CinematicEvents.cs) : Animation 멈추는 Event 구현

--- Assets/Scripts/Event/CinematicEvents.cs
+++ Assets/Scripts/Event/CinematicEvents.cs
@@ -102,3 +102,26 @@ public class ControlShaderEvent : CinematicEvent
         return null;
     }
 }
+public abstract class AnimationControllEvent : CinematicEvent
+{
+    [SerializeField] protected Animator _animator;
+}
+
+public class StopAnimatiorEvent : AnimationControllEvent
+{
+    public override IEnumerator Execute(EventObject eventObject = null)
+    {
+        Debug.Log(_animator);
+        _animator.enabled = false;
+        return null;
+    }
+}
+
+public class StartAnimatiorEvent : AnimationControllEvent
+{
+    public override IEnumerator Execute(EventObject eventObject = null)
+    {
+        _animator.enabled = true;
+        return null;
+    }
+}
