23b7003ee8a94420521a25998fbe9a0f387197ec
mandu <jys0972@gmail.com>
2024-12-22 16:46:56 +0900

    feat(EventObject.cs) : Event로 Triggable 하도록 구현

--- Assets/Scripts/Event/EventObject.cs
+++ Assets/Scripts/Event/EventObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventObject : InteractableObject, IAttackable
+public class EventObject : InteractableObject, IAttackable, IEventTriggerable
 {
     private enum EventTrigger
     {
