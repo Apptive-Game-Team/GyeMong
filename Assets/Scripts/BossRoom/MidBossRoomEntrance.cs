e8299d3778e80ce17ef3b08cba60a18f2d8d05d7
mandu <jys0972@gmail.com>
2024-12-22 21:06:19 +0900

    feat(MidBossRoomEntrance.cs) : Triggerable하게 수정

--- Assets/Scripts/BossRoom/MidBossRoomEntrance.cs
+++ Assets/Scripts/BossRoom/MidBossRoomEntrance.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MidBossRoomEntrance : MonoBehaviour
+public class MidBossRoomEntrance : MonoBehaviour, IEventTriggerable
 {
     [SerializeField] MidBoss midBoss;
-    public void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
+    // public void OnTriggerExit2D(Collider2D other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     {
+    //         if (midBoss != null && midBoss.curState == Creature.State.NONE)
+    //         {
+    //             midBoss.StartDetectingPlayer();
+    //         }
+    //     }
+    // }
+
+    public void Trigger()
+    { 
+        if (midBoss != null && midBoss.curState == Creature.State.NONE)
         {
-            if (midBoss != null && midBoss.curState == Creature.State.NONE)
-            {
-                midBoss.StartDetectingPlayer();
-            }
+            midBoss.StartDetectingPlayer();
         }
     }
 }
