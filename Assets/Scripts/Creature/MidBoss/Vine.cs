53951c2e364c65e1d84784b6b82bd3aefcb14831
hwanginseop <163392234+hwanginseop@users.noreply.github.com>
2024-12-26 14:34:12 +0900

    refact(vine.cs)
    ontriggerStay로 변경하여 연속적인 데미지 전달

--- Assets/Scripts/Creature/MidBoss/Vine.cs
+++ Assets/Scripts/Creature/MidBoss/Vine.cs
@@ -13,7 +13,7 @@ public class Vine : MonoBehaviour
         yield return new WaitForSeconds(2f);
         Destroy(gameObject);
     }
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
