5f421db9a97dc83df6e083561036c7bd98dab4df
mandu <jys0972@gmail.com>
2024-12-28 14:56:57 +0900

    feat(MeleeAttack.cs) : Null 처리 구현

--- Assets/Scripts/Creature/MidBoss/MeleeAttack.cs
+++ Assets/Scripts/Creature/MidBoss/MeleeAttack.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 
 public class MeleeAttack : MonoBehaviour
 {
-    private float attackdamage = MidBoss.GetInstance<MidBoss>().defaultDamage;
+    public float? attackdamage = MidBoss.GetInstance<MidBoss>()?.defaultDamage;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            PlayerCharacter.Instance.TakeDamage(attackdamage);
+            PlayerCharacter.Instance.TakeDamage(attackdamage.Value);
         }
     }
 }
