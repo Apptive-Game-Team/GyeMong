d39baa6fcddeafc61481d5afaa483f1362c61213
mandu <jys0972@gmail.com>
2024-12-28 15:40:05 +0900

    refact(Guardian.cs)

--- Assets/Scripts/Creature/Guardian/Guardian.cs
+++ Assets/Scripts/Creature/Guardian/Guardian.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
+
 public class Guardian : Boss
 {
     [SerializeField] private GameObject cubePrefab;
