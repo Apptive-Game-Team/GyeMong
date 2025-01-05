b89cd6db05a4c1089d9a389dc5a7b2439bfbf301
mandu <jys0972@gmail.com>
2025-01-05 13:30:44 +0900

    feat(GuardianAttack.cs) : BossAttack으로 설정

--- Assets/Scripts/Creature/Guardian/GuardianAttack.cs
+++ Assets/Scripts/Creature/Guardian/GuardianAttack.cs
@@ -1,23 +1,16 @@
 using playerCharacter;
 using System.Collections;
 using System.Collections.Generic;
+using Rework;
 using UnityEngine;
 
-public class GuardianAttack : MonoBehaviour
+public class GuardianAttack : BossAttack
 {
-    public float? attackdamage = Boss.GetInstance<Guardian>()?.defaultDamage;
-    private void Update()
-    {
-        if (Boss.GetInstance<Guardian>().curState == Creature.State.STUN)
-        {
-            Destroy(gameObject);
-        }
-    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            PlayerCharacter.Instance.TakeDamage(attackdamage.Value);
+            PlayerCharacter.Instance.TakeDamage(damage);
         }
     }
 }
