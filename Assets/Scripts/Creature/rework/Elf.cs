6f144032d0db0444e903e6e9d85242447c529ad0
mandu <jys0972@gmail.com>
2025-01-06 14:47:27 +0900

    feat(Elf.cs) : ElfState, Move, BackStep, RangedAttack 2종 구현

--- Assets/Scripts/Creature/rework/Elf.cs
+++ Assets/Scripts/Creature/rework/Elf.cs
@@ -4,7 +4,6 @@ using TMPro;
 using UnityEngine;
 
 public class Elf : Boss
-{
 { 
     [SerializeField] private GameObject arrowPrefab;
     [SerializeField] private GameObject seedPrefab;
@@ -38,4 +37,103 @@ public class Elf : Boss
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         arrow.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
      }
+    
+    public abstract class ElfState : BaseState
+    {
+        public Elf Elf => creature as Elf;
+    }
+
+    public class MoveState : ElfState
+    {
+        public override int GetWeight()
+        {
+            return (Elf.DistanceToPlayer > Elf.MeleeAttackRange) ? 5 : 0;
+        }
+
+        public override IEnumerator StateCoroutine()
+        {
+            Elf.Animator.SetBool("isMove", true);
+
+            float duration = 2f;
+            float timer = 0f;
+            
+            while (duration > timer && Elf.DistanceToPlayer > Elf.MeleeAttackRange)
+            {
+                timer += Time.deltaTime;
+                yield return null;
+                Elf.TrackPlayer();
+                Elf.Animator.SetFloat("xDir", Elf.DirectionToPlayer.x);
+                Elf.Animator.SetFloat("yDir", Elf.DirectionToPlayer.y);
+            }
+            
+            Elf.Animator.SetBool("isMove", false);
+            Elf.ChangeState();
+        }
+    }
+    
+    public new class BackStep : ElfState
+    {
+        public override int GetWeight()
+        {
+            return (Elf.DistanceToPlayer < Elf.RangedAttackRange / 2) ? 5 : 0;
+        }
+
+        public override IEnumerator StateCoroutine()
+        {
+            Elf.Animator.SetBool("isDash", true);
+
+            Elf.Animator.SetFloat("dashType", 1);
+            Elf.Animator.SetFloat("xDir", Elf.DirectionToPlayer.x);
+            Elf.Animator.SetFloat("yDir", Elf.DirectionToPlayer.y);
+            yield return Elf.BackStep(Elf.RangedAttackRange);
+            
+            Elf.Animator.SetBool("isDash", false);
+            Elf.ChangeState();
+        }
+    }
+
+    public class RangedAttack : ElfState
+    {
+        public override int GetWeight()
+        {
+            return (Elf.DistanceToPlayer > Elf.RangedAttackRange/2) ? 5 : 0;
+        }
+
+        public override IEnumerator StateCoroutine()
+        {
+             yield return new WaitForSeconds(0.5f);
+             GameObject arrow =  Instantiate(Elf.arrowPrefab, Elf.transform.position, Quaternion.identity);
+             Elf.RotateArrowTowardsPlayer(arrow);
+             yield return Elf.arrowSoundObject.Play();
+             yield return new WaitForSeconds(1f);
+             Elf.ChangeState();
+        }
+    }
+    public class SeedRangedAttak : ElfState
+    {
+        public override int GetWeight()
+        {
+            if (Elf.CurrentPhase == 1)
+            {
+                return (Elf.DistanceToPlayer > Elf.RangedAttackRange / 2) ? 5 : 0;
+            }
+            return 0;
+        }
+
+        public override IEnumerator StateCoroutine()
+        {
+            yield return new WaitForSeconds(1f); 
+            int count = 0;
+            while (count < 4)
+            {
+                GameObject seed = Instantiate(Elf.seedPrefab, Elf.transform.position, Quaternion.identity);
+                Elf.RotateArrowTowardsPlayer(seed);
+                yield return Elf.arrowSoundObject.Play();
+                count++;
+            }
+            Elf.ChangeState();
+        }
+    }
 }
+
+
