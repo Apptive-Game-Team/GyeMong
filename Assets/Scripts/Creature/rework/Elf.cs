a7b548822bc357b5d45265b87689ea23eed90f99
mandu <jys0972@gmail.com>
2025-01-06 14:45:44 +0900

    feat(Elf.cs) : 초기화 로직, 기본 화살 다루는 함수 구현

--- Assets/Scripts/Creature/rework/Elf.cs
+++ Assets/Scripts/Creature/rework/Elf.cs
@@ -1,4 +1,41 @@
+using System.Collections;
+using playerCharacter;
+using TMPro;
+using UnityEngine;
 
 public class Elf : Boss
 {
+{ 
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private GameObject seedPrefab;
+    [SerializeField] private GameObject vinePrefab;
+    [SerializeField] private GameObject meleeAttackPrefab;
+    Vector3 meleeAttackPrefabPos;
+    
+    private FootSoundController footSoundController;
+    [SerializeField] private SoundObject arrowSoundObject;
+    [SerializeField] private SoundObject vineSoundObject;
+    
+    protected override void Initialize()
+    {
+         maxPhase = 2;
+         maxHps.Add(100f);
+         maxHps.Add(200f);
+         currentHp = maxHps[currentPhase];
+         damage = 20f;
+         speed = 2f;
+         currentShield = 0f;
+         detectionRange = 10f;
+         MeleeAttackRange = 2f;
+         RangedAttackRange = 6f;
+         meleeAttackPrefab.SetActive(false);
+         footSoundController = transform.Find("FootSoundObject").GetComponent<FootSoundController>();
+    }
+    
+    private void RotateArrowTowardsPlayer(GameObject arrow)
+    {
+        Vector3 direction = DirectionToPlayer; 
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        arrow.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+     }
 }
