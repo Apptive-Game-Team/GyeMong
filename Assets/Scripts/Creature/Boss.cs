5ac6b099abe6dadb18cd1dda126f03ced5610504
hwanginseop <163392234+hwanginseop@users.noreply.github.com>
2025-01-16 17:12:57 +0900

    refact(Boss)
    Directory 정리

--- Assets/Scripts/Creature/Boss.cs
+++ Assets/Scripts/Creature/Boss.cs
@@ -1,229 +1,88 @@
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-//
-// public abstract class Boss : Creature
-// {
-//     private static Dictionary<Type, Boss> _instances = new Dictionary<Type, Boss>();
-//     
-//     public static T GetInstance<T>() where T : Boss
-//     {
-//         Type type = typeof(T);
-//         if (_instances.TryGetValue(type, out Boss instance))
-//         {
-//             return instance as T;
-//         }
-//         return null;
-//     }
-//     
-//     protected override void Awake()
-//     {
-//         base.Awake();
-//         
-//         Type type = GetType();
-//         
-//         if (_instances.ContainsKey(type) && _instances[type] != this)
-//         {
-//             Destroy(gameObject);
-//             return;
-//         }
-//         
-//         _instances[type] = this;
-//     }
-//
-//     
-//     public GameObject wall;
-//     public GameObject mapPattern;
-//     private Coroutine detectPlayerRoutine;
-//     protected int currentPhase = 1;
-//     protected int maxPhase;
-//     protected float maxHealthP1;
-//     protected float maxHealthP2;
-//
-//     public float CurrentHp
-//     {
-//         get { return curHealth; }
-//     }
-//
-//     public float CurrentShield
-//     {
-//         get { return shield; }
-//     }
-//
-//     public int CurrentPhase
-//     {
-//         get { return currentPhase; }
-//     }
-//     public Tuple<int, float, float> BossInfo
-//     {
-//         get { return new Tuple<int, float, float>(currentPhase, maxHealthP1, maxHealthP2); }
-//     }
-//     protected int lastPattern = -1;
-//     public override void TakeDamage(float damage)
-//     {
-//         base.TakeDamage(damage);
-//         CheckPhaseTransition();
-//     }
-//     protected void SetupPhase()
-//     {
-//         switch (currentPhase)
-//         {
-//             case 1:
-//                 curHealth = maxHealthP1;
-//                 break;
-//             case 2:
-//                 curHealth = maxHealthP2;
-//                 break;
-//             default:
-//                 curHealth = 200f;
-//                 break;
-//         }
-//     }
-//     protected void CheckPhaseTransition()
-//     {
-//         if (curHealth <= 0)
-//         {
-//             TransPhase();
-//         }
-//     }
-//     protected void TransPhase()
-//     {
-//         if (currentPhase < maxPhase)
-//         {
-//             currentPhase++;
-//             StopAllCoroutines();
-//             gameObject.GetComponent<Renderer>().material.SetFloat("_BlinkTrigger", 0f);
-//             StartCoroutine(ChangingPhase());
-//         }
-//         else
-//         {
-//             Die();
-//             wall.SetActive(false);
-//             mapPattern.SetActive(false);
-//         }
-//     }
-//     public IEnumerator ChangingPhase()
-//     {
-//         ChangeState(State.CHANGINGPHASE);
-//         SetupPhase();
-//         GameObject.Find("PhaseChangeObj").GetComponent<EventObject>().Trigger();
-//         yield return new WaitForSeconds(2f);
-//         ChangeState(State.IDLE);
-//     }
-//     public Coroutine currentPatternCoroutine;
-//     protected void ExecuteCurrentPattern()
-//     {
-//         if (currentPatternCoroutine != null) return; // �ߺ� ���� ����
-//         switch (curPattern)
-//         {
-//             case 0:
-//                 currentPatternCoroutine = StartCoroutine(ExecutePattern0());
-//                 break;
-//             case 1:
-//                 currentPatternCoroutine = StartCoroutine(ExecutePattern1());
-//                 break;
-//             case 2:
-//                 currentPatternCoroutine = StartCoroutine(ExecutePattern2());
-//                 break;
-//             case 3:
-//                 currentPatternCoroutine = StartCoroutine(ExecutePattern3());
-//                 break;
-//             case 4:
-//                 currentPatternCoroutine = StartCoroutine(ExecutePattern4());
-//                 break;
-//             case 5:
-//                 currentPatternCoroutine = StartCoroutine(ExecutePattern5());
-//                 break;
-//         }
-//     } //���� ���� �߰��� case�߰�
-//     protected abstract IEnumerator ExecutePattern0();
-//     protected abstract IEnumerator ExecutePattern1();
-//     protected abstract IEnumerator ExecutePattern2();
-//     protected abstract IEnumerator ExecutePattern3();
-//     protected abstract IEnumerator ExecutePattern4();
-//     protected abstract IEnumerator ExecutePattern5();
-//     protected abstract void SelectRandomPattern();//���� ���� �ż��� �߻�ȭ
-//     public void TrackPlayer()
-//     {
-//         if (player != null)
-//         {
-//             float step = speed * Time.deltaTime;
-//             Vector3 targetPosition = player.transform.position;
-//             Vector3 newPosition = Vector3.MoveTowards(transform.position, targetPosition, step);
-//             transform.position = newPosition;
-//         }
-//     }
-//     public IEnumerator BackStep(float targetDistance)
-//     {
-//         if (player != null)
-//         {
-//             Vector3 playerPosition = player.transform.position;
-//             float backStepSpeed = 50;
-//             Vector3 direction = (transform.position - playerPosition).normalized;
-//             Rigidbody2D rb = GetComponent<Rigidbody2D>();
-//             LayerMask obstacleLayer = LayerMask.GetMask("Obstacle");
-//             float currentDistance = Vector3.Distance(transform.position, playerPosition);
-//             RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, targetDistance, obstacleLayer);
-//             int count=0;
-//             while (hit.collider != null && count<360)
-//             {
-//                 float angle = UnityEngine.Random.Range(0,360f);
-//                 direction = Quaternion.Euler(0, 0, angle) * direction;
-//                 hit = Physics2D.Raycast(transform.position, direction, targetDistance, obstacleLayer);
-//                 count++;
-//             }
-//             if(hit.collider == null)
-//             {
-//                 currentDistance = targetDistance;
-//                 count = 0;
-//                 float deltaTime = 0.02f;
-//                 while (currentDistance > 0 && count < 100000)
-//                 {
-//                     Vector3 deltaDistance = direction * backStepSpeed * deltaTime;
-//                     currentDistance -= deltaDistance.magnitude;
-//                     Vector3 newPosition = transform.position + deltaDistance;
-//                     rb.MovePosition(newPosition);
-//                     count++;
-//                     yield return new WaitForSeconds(deltaTime);
-//                 }
-//             }
-//             else
-//             {
-//                 yield return null;
-//             }
-//         }
-//     }
-//     public IEnumerator DetectPlayerCoroutine()
-//     {
-//         while (true)
-//         {
-//             float distance = Vector3.Distance(transform.position, player.transform.position);
-//             if (distance <= detectionRange)
-//             {
-//                 wall.SetActive(true);
-//                 StopDetectingPlayer();
-//                 ChangeState(State.IDLE);
-//                 yield break;
-//             }
-//             yield return new WaitForSeconds(0.5f);
-//         }
-//     }
-//     public void StartDetectingPlayer()
-//     {
-//         if (detectPlayerRoutine == null)
-//         {
-//             detectPlayerRoutine = StartCoroutine(DetectPlayerCoroutine());
-//         }
-//     }
-//
-//     public void StopDetectingPlayer()
-//     {
-//         if (detectPlayerRoutine != null)
-//         {
-//             StopCoroutine(detectPlayerRoutine);
-//             detectPlayerRoutine = null;
-//         }
-//     }
-// }
-//
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Boss : Creature
+{
+    protected int currentPhase = 0;
+    protected int maxPhase;
+    protected List<float> maxHps = new List<float>();
+    
+    public int CurrentPhase {get { return currentPhase; }}
+    public float CurrentMaxHp {get {
+        try
+        {
+            return maxHps[currentPhase];
+        }
+        catch (Exception)
+        {
+            return 100;
+        }
+    }}
+
+    protected void Start()
+    {
+        Initialize();
+    }
+
+    protected abstract void Initialize();
+
+    public override void OnAttacked(float damage)
+    {
+        base.OnAttacked(damage);
+        CheckPhaseTransition();
+    }
+    
+    protected void CheckPhaseTransition()
+    {
+        if (currentHp <= 0)
+        {
+            TransPhase();
+        }
+    }
+    
+    protected void TransPhase()
+    {
+        if (currentPhase < maxHps.Count-1)
+        {
+            currentPhase++;
+            StopAllCoroutines();
+            MaterialController.SetMaterial(MaterialController.MaterialType.DEFAULT);
+            StartCoroutine(ChangingPhase());
+        }
+        else
+        {
+            MaterialController.SetMaterial(MaterialController.MaterialType.DEFAULT);
+            Die();
+        }
+    }
+
+    public IEnumerator ChangingPhase()
+    {
+        currentHp = CurrentMaxHp;
+        GameObject.Find("PhaseChangeObj").GetComponent<EventObject>().Trigger();
+        yield return new WaitForSeconds(2f);
+        ChangeState();
+     }
+    
+    public override IEnumerator Stun()
+    {
+        currentShield = 0f;
+        MaterialController.SetMaterial(MaterialController.MaterialType.DEFAULT);
+        yield return base.Stun();
+    }
+    
+    protected void Die() // Boss Clear Event
+    {
+        try
+        {
+            StopAllCoroutines();
+            GameObject.Find("BossDownEventObject").gameObject.GetComponent<EventObject>().Trigger();
+        }
+        catch
+        {
+            Debug.Log("BossDownEventObject not found");
+        }
+    }
+}
--- Assets/Scripts/Creature/Creature.cs
+++ Assets/Scripts/Creature/Creature.cs
@@ -1,169 +1,185 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// public class Creature : MonoBehaviour
-// {
-//     public enum State
-//     {
-//         NONE,
-//         IDLE,
-//         MOVE,
-//         ATTACK,
-//         CHANGINGPATTERN,
-//         ONHIT,
-//         STUN,
-//         DASH,
-//         CHANGINGPHASE
-//     }
-//     protected FiniteStateMachine _fsm;
-//     public State curState;
-//     public int curPattern;
-//     public int dashType;
-//     protected float maxHealth;
-//     [SerializeField] protected float curHealth;
-//     public float defaultDamage;
-//     protected float speed;
-//     protected float detectionRange;
-//     protected float MeleeAttackRange;
-//     protected float RangedAttackRange;
-//     public Vector2 moveDir { get; private set; }
-//     [SerializeField] protected float shield;
-//     protected GameObject player;
-//     public Material[] materials;
-//     private float blinkDelay = 0.15f;
-//
-//     protected virtual void Awake() // Set Default Material
-//     {
-//         gameObject.GetComponent<Renderer>().material = materials[0];
-//     }
-//
-//     protected virtual void Update()
-//     {
-//         Vector3 targetPosition = player.transform.position;
-//         Vector3 direction = targetPosition - transform.position;
-//         SetMoveDirection(new Vector2(direction.x, direction.y));
-//     }
-//     public void SetMoveDirection(Vector2 direction)
-//     {
-//         moveDir = direction;
-//         moveDir.Normalize();
-//     }
-//     public virtual void TakeDamage(float damage)
-//     {
-//         //OnHit();
-//         if (shield >= damage)
-//         {
-//             shield -= damage;
-//         }
-//         else
-//         {
-//             float temp = shield;
-//             shield = 0;
-//             OnShieldBroken();
-//             OnHitEffect();
-//             curHealth -= (damage-temp);
-//         }
-//     }
-//     protected virtual void Die() // Clear Event
-//     {
-//         try
-//         {
-//             GameObject.Find("BossDownEventObject").gameObject.GetComponent<EventObject>().Trigger();
-//         }
-//         catch
-//         {
-//             Debug.Log("BossDownEventObject not found");
-//         }
-//         Destroy(gameObject);
-//     }
-//     public virtual void ChangeState(State nextState)
-//     {
-//         curState = nextState;
-//         switch(curState)    
-//         {
-//             case State.NONE:
-//                 {
-//                     _fsm.ChangeState(new NoneState(this));
-//                     break;
-//                 }
-//             case State.IDLE:
-//                 {
-//                     if (this.GetType() == typeof(Guardian))
-//                     {
-//                         _fsm.ChangeState(new IdleState<Guardian>(this)); 
-//                     }
-//                     else if (this.GetType() == typeof(MidBoss))
-//                     {
-//                         _fsm.ChangeState(new IdleState<MidBoss>(this)); 
-//                     }
-//                     break;
-//                 }
-//             case State.MOVE:
-//                 {
-//                     _fsm.ChangeState(new MoveState(this));
-//                     break;
-//                 }
-//             case State.ATTACK:
-//                 {
-//                     _fsm.ChangeState(new AttackState(this));
-//                     break;
-//                 }
-//             case State.CHANGINGPATTERN:
-//                 {
-//                     _fsm.ChangeState(new ChangingPatternState(this));
-//                     break;
-//                 }
-//             case State.ONHIT:
-//                 {
-//                     _fsm.ChangeState(new OnHitState(this));
-//                     break;
-//                 }
-//             case State.STUN:
-//                 {
-//                     _fsm.ChangeState(new StunState(this));
-//                     break;
-//                 }
-//             case State.DASH:
-//                 {
-//                     _fsm.ChangeState(new DashState(this));
-//                     break;
-//                 }
-//             case State.CHANGINGPHASE:
-//                 {
-//                     _fsm.ChangeState(new ChangingPhaseState(this));
-//                     break;
-//                 }
-//         }
-//     }
-//     public void OnHit()
-//     {
-//         StartCoroutine(OnHitCoroutine());
-//     }
-//
-//     private IEnumerator OnHitCoroutine()
-//     {
-//         State curState_ = curState;
-//         ChangeState(State.ONHIT);
-//         yield return null;
-//         ChangeState(curState_);
-//     }
-//
-//     private void OnHitEffect()
-//     {
-//         StartCoroutine(OnHitEffectCoroutine());
-//     }
-//
-//     private IEnumerator OnHitEffectCoroutine()
-//     {
-//         gameObject.GetComponent<Renderer>().material.SetFloat("_BlinkTrigger", 1f);
-//         yield return new WaitForSeconds(blinkDelay);
-//         gameObject.GetComponent<Renderer>().material.SetFloat("_BlinkTrigger", 0f);
-//     }
-//
-//     protected virtual void OnShieldBroken()
-//     {
-//         gameObject.GetComponent<Renderer>().material.SetFloat("_isShieldActive", 0f);
-//         gameObject.GetComponent<Renderer>().material = materials[0];
-//     }
-// }
-//
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using playerCharacter;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public abstract class Creature : MonoBehaviour, IAttackable
+{
+    private const float BLINK_DELAY = 0.15f;
+    
+    protected float maxHp;
+    protected float currentHp;
+    public float CurrentHp {get { return currentHp; }}
+    public float currentShield;
+    public float CurrentShield {get { return currentShield; }}
+
+    public float damage;
+    
+    protected float speed;
+    protected float detectionRange;
+    public float MeleeAttackRange {get; protected set;}
+    public float RangedAttackRange {get; protected set;}
+
+    private Coroutine _currentStateCoroutine;
+
+    protected Animator _animator;
+    private MaterialController _materialController;
+    public MaterialController MaterialController
+    {
+        get
+        {
+            if (_materialController == null)
+            {
+                _materialController = GetComponent<MaterialController>();
+            }
+            return _materialController;
+        }
+    }
+    public Animator Animator
+    {
+        get
+        {
+            if (_animator == null)
+            {
+                _animator = GetComponent<Animator>();
+            }
+            return _animator;
+        }
+    }
+    
+    public float DistanceToPlayer => Vector3.Distance(transform.position, PlayerCharacter.Instance.transform.position);
+    public Vector3 DirectionToPlayer => (PlayerCharacter.Instance.transform.position - transform.position).normalized;
+
+    public void ChangeState()
+    {
+        if (_currentStateCoroutine != null)
+            StopCoroutine(_currentStateCoroutine);
+        
+        BaseState[] states = States;
+        List<int> weights = new();
+        int index = 0;
+        int randomIndex;
+        foreach (BaseState state in states)
+        {
+            weights.AddRange(Enumerable.Repeat(index++, state.GetWeight()));
+        }
+        randomIndex = Random.Range(0, weights.Count);
+        _currentStateCoroutine = StartCoroutine(states[weights[randomIndex]].StateCoroutine());
+    }
+    
+    protected IEnumerator Blink()
+    {
+        MaterialController.SetMaterial(MaterialController.MaterialType.HIT);
+        MaterialController.SetFloat(1);
+        yield return new WaitForSeconds(BLINK_DELAY);
+        if (MaterialController.GetCurrentMaterialType() == MaterialController.MaterialType.HIT)
+        {
+            MaterialController.SetFloat(0);
+        }
+    }
+    
+    public virtual IEnumerator Stun()
+    {
+         StopCoroutine(_currentStateCoroutine);
+         yield return new WaitForSeconds(5f);
+         ChangeState();
+    }
+    
+    public void TrackPlayer()
+    {
+        float step = speed * Time.deltaTime;
+        Vector3 targetPosition = PlayerCharacter.Instance.transform.position;
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, targetPosition, step);
+        transform.position = newPosition;
+    }
+    
+    public IEnumerator BackStep(float targetDistance)
+     {
+         Vector3 playerPosition = PlayerCharacter.Instance.transform.position;
+         float backStepSpeed = 50;
+         Vector3 direction = (transform.position - playerPosition).normalized;
+         Rigidbody2D rb = GetComponent<Rigidbody2D>();
+         LayerMask obstacleLayer = LayerMask.GetMask("Obstacle");
+         float currentDistance = Vector3.Distance(transform.position, playerPosition);
+         RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, targetDistance, obstacleLayer);
+         int count=0;
+         while (hit.collider != null && count<360)
+         {
+             float angle = UnityEngine.Random.Range(0,360f);
+             direction = Quaternion.Euler(0, 0, angle) * direction;
+             hit = Physics2D.Raycast(transform.position, direction, targetDistance, obstacleLayer);
+             count++;
+         }
+         if(hit.collider == null)
+         {
+             currentDistance = targetDistance;
+             count = 0;
+             float deltaTime = 0.02f;
+             while (currentDistance > 0 && count < 100000)
+             {
+                 Vector3 deltaDistance = direction * backStepSpeed * deltaTime;
+                 currentDistance -= deltaDistance.magnitude;
+                 Vector3 newPosition = transform.position + deltaDistance;
+                 rb.MovePosition(newPosition);
+                 count++;
+                 yield return new WaitForSeconds(deltaTime);
+             }
+         }
+         else
+         {
+             yield return null;
+         }
+     }
+
+    public abstract class BaseState
+    {
+        public Creature creature;
+        public abstract int GetWeight();
+
+        public abstract IEnumerator StateCoroutine();
+    }
+    
+    private BaseState[] _states;
+    public BaseState[] States
+    {
+        get
+        {
+            if (_states == null)
+            {
+                List<BaseState> states = new List<BaseState>();
+                Type parentType = GetType();
+                Type[] stateTypes = parentType.GetNestedTypes();
+                foreach (Type type in stateTypes)
+                {
+                    if (!type.IsAbstract)
+                    {
+                        states.Add(Activator.CreateInstance(type) as BaseState);
+                        states[states.Count - 1].creature = this;
+                    }
+                }
+                _states = states.ToArray();
+            }
+            return _states;
+        }
+    }
+
+    public virtual void OnAttacked(float damage)
+    {
+        if (currentShield >= damage)
+         {
+             currentShield -= damage;
+         }
+         else
+         {
+             float temp = currentShield;
+             currentShield = 0;
+             MaterialController.SetMaterial(MaterialController.MaterialType.DEFAULT);
+             StartCoroutine(Blink());
+             currentHp -= (damage-temp);
+         }
+    }
+}
+
