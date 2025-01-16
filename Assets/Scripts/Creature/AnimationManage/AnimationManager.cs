180faa774e0eb26bcad6b586008227453d5435b3
hwanginseop <163392234+hwanginseop@users.noreply.github.com>
2025-01-16 18:53:59 +0900

    feat(AnimationDataList.cs)
    애니메이션 관리 scriptableObj 생성

---
+++ Assets/Scripts/Creature/AnimationManage/AnimationManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationManager : MonoBehaviour
+{
+    [SerializeField] AnimationDataList animationDataList;
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    /*public IEnumerator TransitScene(PortalID portalID)
+    {
+        yield return EffectManager.Instance.FadeOut();
+        PortalData portalData = portalDataList.GetPortalDataByID(portalID);
+        SceneData sceneData = sceneDataList.GetSceneDataByID(portalData.sceneID);
+        SceneManager.LoadScene(sceneData.sceneName);
+        playerCharacter.PlayerCharacter.Instance.transform.position = portalData.destination;
+        StartCoroutine(EffectManager.Instance.FadeIn());
+    }*/
+}
