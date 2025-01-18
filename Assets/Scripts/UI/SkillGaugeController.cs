6f01590b762218cdd7894faaf8731f0b99528e18
191012 <kimhj191012@naver.com>
2025-01-18 16:20:36 +0900

    feat(SkillGaugeController.cs) : 스킬 게이지 쉐이더 적용

--- Assets/Scripts/UI/SkillGaugeController.cs
+++ Assets/Scripts/UI/SkillGaugeController.cs
@@ -7,10 +7,12 @@ using UnityEngine.UI;
 public class SkillGaugeController : MonoBehaviour
 {
     private Slider skillSlider;
+    private Material gaugeEffectMaterial;
 
     private void Awake()
     {
         skillSlider = GetComponent<Slider>();
+        gaugeEffectMaterial = transform.Find("Background").GetComponent<Image>().material;
     }
 
     private void Update()
@@ -21,5 +23,13 @@ public class SkillGaugeController : MonoBehaviour
     private void UpdateSkillGauge()
     {
         skillSlider.value = PlayerCharacter.Instance.GetCurSkillGauge() / PlayerCharacter.Instance.maxSkillGauge;
+        if (PlayerCharacter.Instance.GetCurSkillGauge() > PlayerCharacter.Instance.skillUsageGauge)
+        {
+            gaugeEffectMaterial.SetFloat("_isUsable", 1);
+        }
+        else
+        {
+            gaugeEffectMaterial.SetFloat("_isUsable", 0);
+        }
     }
 }
