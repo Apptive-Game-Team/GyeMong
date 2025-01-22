46dd57918f546babdfd4362f1c05d58e71c58c90
mandu <jys0972@gmail.com>
2025-01-22 16:15:09 +0900

    feat(CurveTreeLineDrawer.cs) : amount of curves 변수로 관리

--- Assets/Scripts/Rune/RuneTreeSystem/CurveTreeLineDrawer.cs
+++ Assets/Scripts/Rune/RuneTreeSystem/CurveTreeLineDrawer.cs
@@ -5,6 +5,8 @@ namespace runeSystem.RuneTreeSystem
 {
     public class CurveTreeLineDrawer : CurvesDrawer, ITreeLineDrawer
     {
+        [SerializeField] private float amountOfCurves = 150;
+        
         public void ClearLines()
         {
             Clear();
@@ -12,7 +14,7 @@ namespace runeSystem.RuneTreeSystem
 
         public void ConnectNodes(Vector2 position1, Vector2 position2)
         {
-            AddCurve(new Vector2[]{position1, position1 + new Vector2(150, 0) , position2 - new Vector2(150, 0), position2});
+            AddCurve(new Vector2[]{position1, position1 + new Vector2(amountOfCurves, 0) , position2 - new Vector2(amountOfCurves, 0), position2});
         }
     }
 }
