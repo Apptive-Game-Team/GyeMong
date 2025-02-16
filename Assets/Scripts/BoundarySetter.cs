fcaadff26f393b86a0921af01ea5f506b3451a6d
jabgok <127014921+Jinwook700@users.noreply.github.com>
2025-02-16 17:16:48 +0900

    Origin/feat/222 (#231)
    * fix RightRoad2 Camera
    
    BoundarySetter 수정
    
    * fix UnwalkableTile
    
    Golem, RightRoad TilemapCollider 추가
    
    * feat EdgeCollider
    
    BoundarySetter에 추가, 근데 맵마다 전부 해줘야해 ㅎ
    
    * fix SpringPuzzle2 Tilemap
    
    WaterTilemap수정.. 근데 영 아닌거같음
    
    * fix SpringAboveRoad2
    
    Tilemap 안보이는거 해결
    
    * fix RighrRoad1
    
    Tilemap 안보이는거 수정
    
    ---------
    
    Co-authored-by: Monolong <tjdvlf0201@gmail.com>

--- Assets/Scripts/BoundarySetter.cs
+++ Assets/Scripts/BoundarySetter.cs
@@ -6,9 +6,21 @@ using UnityEngine;
 public class BoundarySetter : MonoBehaviour
 {
     [SerializeField] private List<Vector2> boundaryPoints;
-    
+    [SerializeField] private List<Vector2> colliderPoints;
+    private EdgeCollider2D edgeCollider;
+
     public void Start()
     {
         CameraController.BoundaryPoints = boundaryPoints;
+
+        edgeCollider = GetComponent<EdgeCollider2D>();
+        SetBoundaryCollider();
+    }
+
+    void SetBoundaryCollider()
+    {
+        if (colliderPoints.Count < 2) return;
+
+        edgeCollider.points = colliderPoints.ToArray();
     }
 }
