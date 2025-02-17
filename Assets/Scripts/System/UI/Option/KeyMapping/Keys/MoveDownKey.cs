f203d84df4425df6d3bfd07364345a38dd6f5190
191012 <kimhj191012@naver.com>
2025-02-17 17:15:02 +0900

    refact(OptionController.prefab, KeyMapping.cs) : 키매핑 시 버튼 별 코드가 아닌 하나의 스크립트로 버튼이 작동하게 변경
    기존의 추상 클래스에서 ActionCode를 받아와서 키를 설정하는게 아닌 SerializeField를 이용해서 버튼에서 ActionCode를 지정해서 넣어주는 방식으로 변경

--- Assets/Scripts/System/UI/Option/KeyMapping/Keys/MoveDownKey.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MoveDownKey : KeyMapping
-{
-    protected override ActionCode ActionCode => ActionCode.MoveDown;
-}
