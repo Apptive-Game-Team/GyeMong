2065c480076c8d42fcb4308892977ed544bb596d
mandu <jys0972@gmail.com>
2025-02-19 15:30:01 +0900

    refact(MazeThorn.cs) : Obsolete

--- Assets/Scripts/Map/Puzzle/Maze/MazeThorn.cs
+++ Assets/Scripts/Map/Puzzle/Maze/MazeThorn.cs
@@ -1,44 +1,48 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using playerCharacter;
 using UnityEngine;
 
-public class MazeThorn : MonoBehaviour
+namespace Map.Puzzle.Maze
 {
-    private float damage = 1f;
-    private float damageDelay = 1f;
-    private Coroutine damageCoroutine;
-
-    private void OnTriggerEnter2D(Collider2D other) 
+    [Obsolete("This class is no longer used. Will be removed in the future.")]
+    public class MazeThorn : MonoBehaviour
     {
-        if (other.CompareTag("Player"))
+        private float damage = 1f;
+        private float damageDelay = 1f;
+        private Coroutine damageCoroutine;
+
+        private void OnTriggerEnter2D(Collider2D other) 
         {
-            if (damageCoroutine == null)
+            if (other.CompareTag("Player"))
             {
-                damageCoroutine = StartCoroutine(Damage());
+                if (damageCoroutine == null)
+                {
+                    damageCoroutine = StartCoroutine(Damage());
+                }
             }
         }
-    }
 
-    private void OnTriggerExit2D(Collider2D other) 
-    {
-        if (other.CompareTag("Player"))
+        private void OnTriggerExit2D(Collider2D other) 
         {
-            if (damageCoroutine != null)
+            if (other.CompareTag("Player"))
             {
-                StopCoroutine(damageCoroutine);
-                damageCoroutine = null;
+                if (damageCoroutine != null)
+                {
+                    StopCoroutine(damageCoroutine);
+                    damageCoroutine = null;
+                }
             }
         }
-    }
 
 
-    private IEnumerator Damage()
-    {
-        while (true)
+        private IEnumerator Damage()
         {
-            PlayerCharacter.Instance.TakeDamage(damage);
-            yield return new WaitForSeconds(damageDelay);
+            while (true)
+            {
+                PlayerCharacter.Instance.TakeDamage(damage);
+                yield return new WaitForSeconds(damageDelay);
+            }
         }
     }
 }
