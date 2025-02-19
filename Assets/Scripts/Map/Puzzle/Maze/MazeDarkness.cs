260f342796a8a4132e3dd480b1597c8a84137165
mandu <jys0972@gmail.com>
2025-02-19 15:48:01 +0900

    refact(TreeGenerator.cs)

--- Assets/Scripts/Map/Puzzle/Maze/MazeDarkness.cs
+++ Assets/Scripts/Map/Puzzle/Maze/MazeDarkness.cs
@@ -7,17 +7,16 @@ namespace Map.Puzzle.Maze
 {
     public class MazeDarkness : MonoBehaviour, IEventTriggerable
     {
-        private Light2D globalLight;
-        private GameObject playerLight;
-        private GameObject player;
-        private bool isInMaze = false;
-        private const float changeRatio = 0.01f;
+        private Light2D _globalLight;
+        private GameObject _playerLight;
+        private GameObject _player;
+        private bool _isInMaze = false;
 
         private void Start()
         {
-            player = PlayerCharacter.Instance.gameObject;
-            globalLight = GameObject.Find("GlobalLight2D").GetComponent<Light2D>();
-            playerLight = player.transform.Find("PlayerLight").GetComponent<Light2D>().gameObject;
+            _player = PlayerCharacter.Instance.gameObject;
+            _globalLight = GameObject.Find("GlobalLight2D").GetComponent<Light2D>();
+            _playerLight = _player.transform.Find("PlayerLight").GetComponent<Light2D>().gameObject;
         }
 
         private void OnTriggerExit2D(Collider2D other) 
@@ -26,13 +25,13 @@ namespace Map.Puzzle.Maze
             {
                 if (other.CompareTag("Player"))
                 {
-                    bool previousState = isInMaze;
-                    isInMaze = player.transform.position.x < transform.position.x;
+                    bool previousState = _isInMaze;
+                    _isInMaze = _player.transform.position.x < transform.position.x;
 
-                    if (previousState != isInMaze)
+                    if (previousState != _isInMaze)
                     {
                         StopAllCoroutines();
-                        StartCoroutine(ChangeIntensity(isInMaze));
+                        StartCoroutine(ChangeIntensity(_isInMaze));
                     }
                 }
             }
@@ -41,22 +40,22 @@ namespace Map.Puzzle.Maze
         public void Trigger()
         {
             StartCoroutine(ChangeIntensity(false));
-            isInMaze = false;
+            _isInMaze = false;
         }
 
         private IEnumerator ChangeIntensity(bool isInMaze)
         {
             float targetIntensity = isInMaze ? 0f : 1f;
-            float Ratio = 0f;
+            float ratio = 0f;
 
-            playerLight.SetActive(isInMaze);
-            while (Mathf.Abs(globalLight.intensity - targetIntensity) > 0.01f)
+            _playerLight.SetActive(isInMaze);
+            while (Mathf.Abs(_globalLight.intensity - targetIntensity) > 0.01f)
             {
-                Ratio += Time.deltaTime;
-                globalLight.intensity = Mathf.Lerp(globalLight.intensity, targetIntensity, Ratio);
+                ratio += Time.deltaTime;
+                _globalLight.intensity = Mathf.Lerp(_globalLight.intensity, targetIntensity, ratio);
                 yield return new WaitForSeconds(0.05f);
             }
-            globalLight.intensity = targetIntensity;
+            _globalLight.intensity = targetIntensity;
 
             yield return null;
         }
--- Assets/Scripts/Map/Puzzle/Maze/TreeGenerator.cs
+++ Assets/Scripts/Map/Puzzle/Maze/TreeGenerator.cs
@@ -7,53 +7,53 @@ namespace Map.Puzzle.Maze
         [SerializeField] GameObject[] treePrefabs;
         [SerializeField] GameObject grid;
  
-        private float mapMinX = -43;
-        private float mapMaxX = 43;
-        private float mapMinY = -27f;
-        private float mapMaxY = 36;
+        private float _mapMinX = -43;
+        private float _mapMaxX = 43;
+        private float _mapMinY = -27f;
+        private float _mapMaxY = 36;
 
-        private float mazeMinX = -25f;
-        private float mazeMaxX = 26f;
-        private float mazeMinY = -23;
-        private float mazeMaxY = 25;
+        private float _mazeMinX = -25f;
+        private float _mazeMaxX = 26f;
+        private float _mazeMinY = -23;
+        private float _mazeMaxY = 25;
 
-        private float rewardMinX = -35;
-        private float rewardMaxX = -25;
-        private float rewardMinY = -7;
-        private float rewardMaxY = 9;
+        private float _rewardMinX = -35;
+        private float _rewardMaxX = -25;
+        private float _rewardMinY = -7;
+        private float _rewardMaxY = 9;
 
-        private float roadMinX = 0;
-        private float roadMaxX = 43;
-        private float roadMinY = -5;
-        private float roadMaxY = 9;
+        private float _roadMinX = 0;
+        private float _roadMaxX = 43;
+        private float _roadMinY = -5;
+        private float _roadMaxY = 9;
 
-        private float treePlacementChance = 0.35f;
+        private float _treePlacementChance = 0.35f;
 
-        private Vector2Int offset = new Vector2Int(); 
+        private Vector2Int _offset = new Vector2Int(); 
 
         void Start()
         {   
-            offset.x = (int)grid.transform.position.x;
-            offset.y = (int)grid.transform.position.y;
+            _offset.x = (int)grid.transform.position.x;
+            _offset.y = (int)grid.transform.position.y;
             PlaceTrees();
         }
 
         void PlaceTrees()
         {
-            for (int x = (int)mapMinX + offset.x; x <= (int)mapMaxX + offset.x; x++)
+            for (int x = (int)_mapMinX + _offset.x; x <= (int)_mapMaxX + _offset.x; x++)
             {
-                for (int y = (int)mapMinY + offset.y; y <= (int)mapMaxY + offset.y; y++)
+                for (int y = (int)_mapMinY + _offset.y; y <= (int)_mapMaxY + _offset.y; y++)
                 {
                     Vector2 position = new Vector2(x, y);
 
-                    if (IsWithinBounds(position, mazeMinX + offset.x, mazeMaxX+ offset.x, mazeMinY+ offset.y, mazeMaxY + offset.y) || 
-                        IsWithinBounds(position, rewardMinX+ offset.x, rewardMaxX+ offset.x, rewardMinY + offset.y, rewardMaxY + offset.y) ||
-                        IsWithinBounds(position, roadMinX + offset.x, roadMaxX + offset.x, roadMinY + offset.y, roadMaxY + offset.y))
+                    if (IsWithinBounds(position, _mazeMinX + _offset.x, _mazeMaxX+ _offset.x, _mazeMinY+ _offset.y, _mazeMaxY + _offset.y) || 
+                        IsWithinBounds(position, _rewardMinX+ _offset.x, _rewardMaxX+ _offset.x, _rewardMinY + _offset.y, _rewardMaxY + _offset.y) ||
+                        IsWithinBounds(position, _roadMinX + _offset.x, _roadMaxX + _offset.x, _roadMinY + _offset.y, _roadMaxY + _offset.y))
                     {
                         continue;
                     }
 
-                    if (Random.value < treePlacementChance)
+                    if (Random.value < _treePlacementChance)
                     {
                         GameObject treePrefab = treePrefabs[Random.Range(0, treePrefabs.Length)];
                         Instantiate(treePrefab, position, Quaternion.identity,transform);
