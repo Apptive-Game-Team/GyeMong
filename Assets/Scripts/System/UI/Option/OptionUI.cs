9149a07abb084d52c12b10c149dc60f49ede48b9
mandu <jys0972@gmail.com>
2025-02-19 19:27:12 +0900

    refact(OptionUI.cs) : OptionUIToggler와 책임 분배

--- Assets/Scripts/System/UI/Option/OptionUI.cs
+++ Assets/Scripts/System/UI/Option/OptionUI.cs
@@ -7,47 +7,17 @@ namespace System.UI.Option
     public class OptionUI : SingletonObject<OptionUI>
     {
         private Image optionImage;
-        private bool isOptionOpened = false;
         public bool isOptionUITop = true;
 
         private void Start()
         {
             FindOptionImage();
-            optionImage.gameObject.SetActive(isOptionOpened);
-        }
-
-        private void Update()
-        {
-            //if (InputManager.Instance.GetKeyDown(ActionCode.Option))
-            if (isOptionUITop)
-            {
-                if (UnityEngine.Input.GetKeyDown(KeyCode.Escape))
-                {
-                    if (!(SceneManager.GetActiveScene().name == "TitleScene")) OpenOrCloseOption();
-                }
-            }
         }
 
         private void FindOptionImage()
         {
             Transform optionImageTransform = transform.Find("Canvas/OptionUI");
             optionImage = optionImageTransform.GetComponent<Image>();
-        }
-
-
-        public void OpenOrCloseOption()
-        {   
-            isOptionOpened = !isOptionOpened;
-            optionImage.gameObject.SetActive(isOptionOpened);
-        
-            PauseOrResumeGame();
-        }
-
-        private void PauseOrResumeGame()
-        {
-            if (isOptionOpened) Time.timeScale = 0f;
-            else Time.timeScale = 1f;
-            InputManager.Instance.SetActionState(!isOptionOpened);
         }
     }
 }
