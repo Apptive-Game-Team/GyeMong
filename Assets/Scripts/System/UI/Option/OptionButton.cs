a4dfe90566f961aa4fe1a6e5546787c5248cf7c0
191012 <kimhj191012@naver.com>
2025-02-19 14:15:48 +0900

    refact(OptionButton.cs) : 각 버튼에 스크립트를 다는게 아닌 버튼들의 부모가 버튼의 처리를 하도록 변경

---
+++ Assets/Scripts/System/UI/Option/OptionButton.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace UI.Option
+{
+    public enum OptionButtonType
+    {
+        Resume,
+        SoundControl,
+        KeyMapping,
+        OptionExit,
+    }
+
+    public class OptionButton : MonoBehaviour
+    {
+        [SerializeField] Button[] buttons;
+        private Dictionary<string, OptionButtonType> buttonMappings  = new()
+        {
+            { "ResumeButton" , OptionButtonType.Resume },
+            { "SoundControlButton" , OptionButtonType.SoundControl },
+            { "KeyMappingButton" , OptionButtonType.KeyMapping },
+            { "OptionExitButton" , OptionButtonType.OptionExit },
+        };
+        [SerializeField] private GameObject optionExitButton;
+
+        private void Start() 
+        {
+            foreach (Button button in buttons)
+            {
+                if (buttonMappings.TryGetValue(button.name, out OptionButtonType buttonType))
+                {
+                    button.onClick.AddListener(() => OnButtonClick(buttonType));
+                }
+                else
+                {
+                    Debug.LogWarning($"[{button.name}] 버튼에 대한 처리 없음");
+                }
+            }
+        }
+
+        private void OnButtonClick(OptionButtonType buttonType)
+        {
+            switch (buttonType)
+            {
+                case OptionButtonType.Resume:
+                    Resume();
+                    break;
+                case OptionButtonType.SoundControl:
+                    SoundControl();
+                    break;
+                case OptionButtonType.KeyMapping:
+                    KeyMapping();
+                    break;
+                case OptionButtonType.OptionExit:
+                    OptionExit();
+                    break;
+            }
+        }
+
+        private void Resume()
+        {
+            OptionUI.Instance.OpenOrCloseOption();
+        }
+
+        private void SoundControl()
+        {
+            OptionUI.Instance.isOptionUITop = false;
+            SoundControlUI.Instance.OpenSoundControlImage();
+        }
+
+        private void KeyMapping()
+        {
+            OptionUI.Instance.isOptionUITop = false;
+            KeyButtonTexts.Instance.UpdateKeyText();
+            KeyMappingUI.Instance.OpenKeyMappingUI();
+        }
+
+        private void OptionExit()
+        {
+            optionExitButton.SetActive(false);
+            OptionUI.Instance.OpenOrCloseOption();
+            Destroy(playerCharacter.PlayerCharacter.Instance.gameObject);
+            SceneManager.LoadScene("TitleScene");
+        }
+    }
+}
+
+
