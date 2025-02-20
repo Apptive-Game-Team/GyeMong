404ceedd777fe1a5483ab2e4eb790e28451ef8af
mandu <jys0972@gmail.com>
2025-02-20 14:13:39 +0900

    refact(SlimeAnimator.cs) : namespace 추가

--- Assets/Scripts/Creature/Minion/Slime/SlimeAnimator.cs
+++ Assets/Scripts/Creature/Minion/Slime/SlimeAnimator.cs
@@ -1,62 +1,65 @@
 using System.Collections;
 using UnityEngine;
 
-public class SlimeAnimator : MonoBehaviour
+namespace Creature.Minion.Slime
 {
-    public static float ANIMATION_DELTA_TIME = 0.4f;
-    
-    private SpriteRenderer spriteRenderer;
-    private SlimeSprites sprites;
-    private Coroutine currentAnimation;
-    private bool stopCurrentAnimation = false;
-    
-    public static SlimeAnimator Create(GameObject parent, SlimeSprites sprites)
+    public class SlimeAnimator : MonoBehaviour
     {
-        SlimeAnimator animator = parent.gameObject.AddComponent<SlimeAnimator>();
-        animator.spriteRenderer = parent.GetComponent<SpriteRenderer>();
-        animator.sprites = sprites;
-        return animator;
-    }
-
-    public enum AnimationType
-    {
-        IDLE,
-        MELEE_ATTACK,
-        RANGED_ATTACK,
-        DIE
-    }
+        public static float ANIMATION_DELTA_TIME = 0.4f;
     
-    public IEnumerator SyncPlay(AnimationType type, bool loop = false)
-    {
-        do
+        private SpriteRenderer spriteRenderer;
+        private SlimeSprites sprites;
+        private Coroutine currentAnimation;
+        private bool stopCurrentAnimation = false;
+    
+        public static SlimeAnimator Create(GameObject parent, SlimeSprites sprites)
         {
-            yield return PlayerAnimation(sprites.GetSprite(type));
-        } while (loop);
-    }
-    
-
-    
-    public void AsyncPlay(AnimationType type, bool loop = false)
-    {
-        if (currentAnimation != null)
-        {
-            Stop();
+            SlimeAnimator animator = parent.gameObject.AddComponent<SlimeAnimator>();
+            animator.spriteRenderer = parent.GetComponent<SpriteRenderer>();
+            animator.sprites = sprites;
+            return animator;
         }
-        currentAnimation = StartCoroutine(SyncPlay(type, loop));
-    }
-    
-    public void Stop()
-    {
-        StopCoroutine(currentAnimation);
-        currentAnimation = null;
-    }
 
-    private IEnumerator PlayerAnimation(Sprite[] sprites)
-    {
-        for (int i = 0; i < sprites.Length; i++)
+        public enum AnimationType
         {
-            spriteRenderer.sprite = sprites[i];
-            yield return new WaitForSeconds(ANIMATION_DELTA_TIME);
+            IDLE,
+            MELEE_ATTACK,
+            RANGED_ATTACK,
+            DIE
+        }
+    
+        public IEnumerator SyncPlay(AnimationType type, bool loop = false)
+        {
+            do
+            {
+                yield return PlayerAnimation(sprites.GetSprite(type));
+            } while (loop);
+        }
+    
+
+    
+        public void AsyncPlay(AnimationType type, bool loop = false)
+        {
+            if (currentAnimation != null)
+            {
+                Stop();
+            }
+            currentAnimation = StartCoroutine(SyncPlay(type, loop));
+        }
+    
+        public void Stop()
+        {
+            StopCoroutine(currentAnimation);
+            currentAnimation = null;
+        }
+
+        private IEnumerator PlayerAnimation(Sprite[] sprites)
+        {
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                spriteRenderer.sprite = sprites[i];
+                yield return new WaitForSeconds(ANIMATION_DELTA_TIME);
+            }
         }
     }
 }
