783c8d967eb548139682ce203833a25225fda125
191012 <kimhj191012@naver.com>
2025-02-20 16:57:37 +0900

    refact(MazeThorn.cs) : 미로의 가시를 EnemyAttack으로 편입

--- Assets/Scripts/Map/Puzzle/Maze/MazeThorn.cs
+++ Assets/Scripts/Map/Puzzle/Maze/MazeThorn.cs
@@ -8,41 +8,14 @@ namespace Map.Puzzle.Maze
     [Obsolete("This class is no longer used. Will be removed in the future.")]
     public class MazeThorn : MonoBehaviour
     {
-        private float damage = 1f;
+        private float damage = 10f;
         private float damageDelay = 1f;
-        private Coroutine damageCoroutine;
+        private EnemyAttackInfo enemyAttackInfo;
 
-        private void OnTriggerEnter2D(Collider2D other) 
+        private void Awake() 
         {
-            if (other.CompareTag("Player"))
-            {
-                if (damageCoroutine == null)
-                {
-                    damageCoroutine = StartCoroutine(Damage());
-                }
-            }
-        }
-
-        private void OnTriggerExit2D(Collider2D other) 
-        {
-            if (other.CompareTag("Player"))
-            {
-                if (damageCoroutine != null)
-                {
-                    StopCoroutine(damageCoroutine);
-                    damageCoroutine = null;
-                }
-            }
-        }
-
-
-        private IEnumerator Damage()
-        {
-            while (true)
-            {
-                PlayerCharacter.Instance.TakeDamage(damage);
-                yield return new WaitForSeconds(damageDelay);
-            }
+            enemyAttackInfo = gameObject.AddComponent<EnemyAttackInfo>();
+            enemyAttackInfo.Initialize(damage, null, false, false,true, damageDelay); 
         }
     }
 }
