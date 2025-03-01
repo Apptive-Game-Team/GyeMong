d4bcae58a581f8ac0084a8e3021bd831fee56a61
mandu <jys0972@gmail.com>
2025-03-01 13:21:13 +0900

    feat(QuestInfo.cs) : nested class에서 분리

--- Assets/Scripts/System/Game/Quest/Quests/Quest.cs
+++ Assets/Scripts/System/Game/Quest/Quests/Quest.cs
@@ -1,58 +1,66 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace System.Game.Quest.Quests
 {
-    public abstract class Quest
+    [Serializable]
+    public class Quest
     {
-        protected QuestInfo _questInfo;
-        
-        public String ClearConditionKey => _questInfo.questName + "IsCleared";
-        public int NumOfGoals => _questInfo.goals.Count;
-        public int NumOfCompletedGoals => _questInfo.goals.FindAll(goal => goal.isCompleted).Count;
-        public int NumOfUncompletedGoals => _questInfo.goals.FindAll(goal => !goal.isCompleted).Count;
-        public bool IsClear => NumOfGoals == NumOfCompletedGoals;
+        private String _path;
+        public Quest(String path)
+        {
+            _path = path;
+        }
 
         public Quest(QuestInfo questInfo)
         {
             _questInfo = questInfo;
         }
-        
-        public abstract void StartQuest(); // Quest Start Event
-        public abstract void UpdateQuest(); // Quest Update Event
-        public abstract void EndQuest(); // Quest End Event
+        [SerializeReference] private QuestInfo _questInfo;
+        public QuestInfo GetQuestInfo
+        {
+            get
+            {
+                if (_questInfo == null)
+                {
+                    _questInfo = UnityEngine.Resources.Load<QuestInfo>(_path);
+                }
+                return _questInfo;
+            }
+        }
+
+        public String StartConditionKey => GetQuestInfo.questName + "IsStarted";
+        public String ClearConditionKey => GetQuestInfo.questName + "IsCleared";
+        public int NumOfGoals => GetQuestInfo.goals.Count;
+        public int NumOfCompletedGoals => GetQuestInfo.goals.FindAll(goal => goal.isCompleted).Count;
+        public int NumOfUncompletedGoals => GetQuestInfo.goals.FindAll(goal => !goal.isCompleted).Count;
+        public bool IsClear => NumOfGoals == NumOfCompletedGoals;
+
+        public void StartQuest()
+        {
+            ConditionManager.Instance.Conditions[StartConditionKey] = true;
+        }
+        // public abstract void UpdateQuest(); // Quest Update Event
+        public void EndQuest()
+        {
+            ConditionManager.Instance.Conditions[ClearConditionKey] = true;
+        }
         
         public void ClearGoal(int index)
         {
-            Goal goal = _questInfo.goals[index];
+            Goal goal = GetQuestInfo.goals[index];
             goal.isCompleted = true;
-            _questInfo.goals[index] = goal;
+            GetQuestInfo.goals[index] = goal;
+
+            if (IsClear)
+            {
+                EndQuest();
+            }
         }
         
         public bool IsClearGoal(int index)
         {
-            return _questInfo.goals[index].isCompleted;
-        }
-        
-        
-        public enum QuestType
-        {
-            MAIN = 0,
-            SUB = 1
-        }
-        public struct QuestInfo
-        {
-            public int questID;
-            public string questName;
-            public string questDescription;
-            public QuestType questType;
-            public List<Goal> goals;
-        }
-
-        public struct Goal // Quest's Goal
-        {
-            public string goalName;
-            public string goalDescription;
-            public bool isCompleted;
+            return GetQuestInfo.goals[index].isCompleted;
         }
     }
 }
