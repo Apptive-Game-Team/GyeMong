18ee439ba78cc81813d861f4a05cfea001eaf828
mandu <jys0972@gmail.com>
2025-03-02 14:54:08 +0900

    feat(PopupWindowEvents.cs) : skippable한 popup window event 구현

--- Assets/Scripts/System/Event/Event/PopupWindowEvents.cs
+++ Assets/Scripts/System/Event/Event/PopupWindowEvents.cs
@@ -12,7 +12,7 @@ namespace System.Event.Event
     {
         [SerializeField] private string _title;
         [SerializeField] private string _message;
-    
+        
         public override IEnumerator Execute(EventObject eventObject = null)
         {
             return PopupWindowController.Instance.OpenPopupWindow(_title, _message);
@@ -24,6 +24,26 @@ namespace System.Event.Event
         public override IEnumerator Execute(EventObject eventObject = null)
         {
             return PopupWindowController.Instance.ClosePopupWindow();
+        }
+    }
+    
+    public class SkippablePopupWindowEvent : PopupWindowEvent
+    {
+        [SerializeField] private string _title;
+        [SerializeField] private string _message;
+        [SerializeField] private float _duration;
+    
+        public override IEnumerator Execute(EventObject eventObject = null)
+        {
+            yield return PopupWindowController.Instance.OpenPopupWindow(_title, _message);
+            
+            float timer = Time.time;
+            yield return new WaitUntil(() =>
+            {
+                return (timer + _duration < Time.time) || InputManager.Instance.GetKeyDown(ActionCode.Interaction);
+            });
+            
+            yield return PopupWindowController.Instance.ClosePopupWindow();
         }
     }
 }
