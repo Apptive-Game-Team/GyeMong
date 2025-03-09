276aabde4fc7c0e7b7098ba1161b4923ee5f7002
Gimlocal <127363458+Gimlocal@users.noreply.github.com>
2025-03-09 15:16:15 +0900

    Feat/262 (#267)
    * feat(DivisionSlimeTest.unity) : 분열 슬라임 테스트용 씬
    
    * feat(EventScene.cs) : 이벤트 연출을 위한 이벤트
    
    * feat(DivisionSlimeTest.unity) : 분열 슬라임 조우 이벤트
    
    * feat(DOTween) : 부드러운 움직임을 위한 에셋 Dotween 추가
    
    * refact(EventScenes.cs) : 로직을 Coroutine으로 변경
    
    * feat(DivisionSlime.cs) : 분열 슬라임 분열 구현
    
    * refact(DivisionSlime, EventScene.cs) : 변수 수치 조정 및 코드 refact
    
    * refact(DivisionSlime.cs) : 분열 슬라임 creture 상속, 크기 조정 오류 수정
    
    * feat(EventScene.cs) : 크기가 커지는 효과 추가
    
    * feat(DivisionSlime.Prefab) : 분열 슬라임 프리팹화
    
    * feat(RangedAttack.cs) : 슬라임 원거리 공격
    
    * feat(DivisionSlime.cs) : 분열 슬라임 간단한 패턴 추가
    
    * refact(DivisionSlime.cs) : 근접공격 로직 변경
    
    * refact(DivisionSlime.cs) : 원거리 공격이 슬라임의 자식으로 들어가게 수정
    
    * refact(RangedAttack.cs) : 원거리 공격 대미지를 분열 개체에 맞게 설정되게 변경
    
    * Update DivisionSlime.cs
    
    * feat(MiniSlime.prefab) : 합쳐지는 슬라임 프리팹 생성
    
    * feat(EventScenes.cs) : 합쳐지는 슬라임 방향 전환 추가
    
    * fix(DivisionSlime.cs, RangedAttack.cs) : 오류 수정
    
    * feat(DivisionSlime) : 피격 대미지 추가 및 대쉬 위치 조정
    
    * refact(DivisionSlime) : EnemyAttackInfo 직접 프리팹에 달기
    
    * fix(DivisionSlime.cs) : 패턴 딜레이 상수화
    
    * feat(DivisionSlime.prefab) : SlimeAnimator를 사용하도록 수정
    
    * refact(DivisionSlime.cs) : SlimeAnimator를 사용하도록 수정
    
    * feat(SlimeAnimator.cs) : 현재 돌아가는 animation 타입 저장
    
    * Update EventScenes.cs
    
    ---------
    
    Co-authored-by: mandu <jys0972@gmail.com>

---
+++ Assets/Scripts/Creature/Minion/Slime/RangedAttack.cs
@@ -0,0 +1,51 @@
+using playerCharacter;
+using System.Collections;
+using UnityEngine;
+using Creature.Boss;
+
+namespace Creature.Minion.Slime
+{
+    public class RangedAttack : MonoBehaviour
+    {
+        private GameObject player;
+        private Vector3 direction;
+        private float speed;
+        private float damage;
+        private EnemyAttackInfo enemyAttackInfo;
+
+        private void Awake()
+        {
+            player = PlayerCharacter.Instance.gameObject;
+            direction = (player.transform.position - transform.position).normalized;
+            RotateArrow();
+            
+            speed = 15f;
+            damage = transform.parent.GetComponent<DivisionSlime>().damage;
+
+            enemyAttackInfo = gameObject.AddComponent<EnemyAttackInfo>();
+            enemyAttackInfo.Initialize(damage, null, true, true);
+        }
+
+        private void OnEnable()
+        {
+            StartCoroutine(FireArrow());
+        }
+        private void RotateArrow()
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
+        private IEnumerator FireArrow()
+        {
+            Vector3 firePosition = transform.position;
+            float fireDistance = 0;
+            while (fireDistance < 10f)
+            {
+                transform.position += direction * speed * Time.deltaTime;
+                fireDistance = Vector3.Distance(firePosition, transform.position);
+                yield return null;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
