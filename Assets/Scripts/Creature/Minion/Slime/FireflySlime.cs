d67a960f8264b90e0dc44af7f2853fe99f4a9607
mandu <jys0972@gmail.com>
2025-03-09 22:10:57 +0900

    fix(SlimeBase.cs) : detector를 통해서 player를 관찰하고 공격하도록 수정, 거리 체크해서 공격 데미지 넣도록 수정

--- Assets/Scripts/Creature/Minion/Slime/FireflySlime.cs
+++ Assets/Scripts/Creature/Minion/Slime/FireflySlime.cs
@@ -70,5 +70,6 @@ namespace Creature.Minion.Slime
         public class SlimeMeleeAttackState : MeleeAttackState {}
         public class DieState : SlimeDieState { }
         public class MoveState : SlimeMoveState { }
+        public class SlimeIdleState : IdleState { }
     }
 }
