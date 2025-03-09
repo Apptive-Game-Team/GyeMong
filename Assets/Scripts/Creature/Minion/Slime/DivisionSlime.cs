5fac63fbb9784516f315401b1db4df1864f8ccdd
mandu <jys0972@gmail.com>
2025-03-09 15:12:33 +0900

    refact(DivisionSlime.cs) : SlimeAnimator를 사용하도록 수정

--- Assets/Scripts/Creature/Minion/Slime/DivisionSlime.cs
+++ Assets/Scripts/Creature/Minion/Slime/DivisionSlime.cs
@@ -11,7 +11,6 @@ namespace Creature.Minion.Slime
     {
         private const float divideRatio = 0.6f;
         private const float damageRatio = 0.5f;
-        public GameObject slimePrefab;
         private int divisionLevel;
         private int maxDivisionLevel;
         private float moveSpeed;
@@ -26,7 +25,11 @@ namespace Creature.Minion.Slime
         private Coroutine startCoroutine;
         private Coroutine curCoroutine;
         private bool isMove = true;
+        private SlimeAnimator _slimeAnimator;
+        [SerializeField] private SlimeSprites sprites;
 
+        private bool _isStart = false;
+        
         private void Awake()
         {
             Initialize();
@@ -34,11 +37,12 @@ namespace Creature.Minion.Slime
 
         private void Initialize()
         {
-            animator = GetComponent<Animator>();
+            _slimeAnimator = GetComponent<SlimeAnimator>();
+            
             currentHp = 10f;
 
-            slimePrefab = gameObject;
-
+            _isStart = false;
+            
             divisionLevel = 0;
             maxDivisionLevel = 2;
             MeleeAttackRange = 4f;
@@ -56,14 +60,18 @@ namespace Creature.Minion.Slime
             damage = 10f;
         }
 
-        private void Start()
+        public void StartMob()
         {
             target = PlayerCharacter.Instance.gameObject.transform;
             startCoroutine = StartCoroutine(Patterns());
+            _slimeAnimator = SlimeAnimator.Create(gameObject, sprites);
+            _isStart = true;
         }
 
         private void Update()
         {
+            if (!_isStart) return;
+            
             if (target == null) return;
             if (isMove)
             {
@@ -74,6 +82,9 @@ namespace Creature.Minion.Slime
 
         private void MoveTowardsTarget()
         {
+            if (_slimeAnimator.CurrentAnimationType != SlimeAnimator.AnimationType.MELEE_ATTACK)
+                _slimeAnimator.AsyncPlay(SlimeAnimator.AnimationType.MELEE_ATTACK, true);
+            
             Vector3 direction = (target.position - transform.position).normalized;
             transform.position += direction * moveSpeed * Time.deltaTime;
         }
@@ -130,15 +141,16 @@ namespace Creature.Minion.Slime
         {
             if (curCoroutine != null) StopCoroutine(curCoroutine);
             isMove = false;
-            animator.SetTrigger("MeleeCharge");
-            yield return new WaitForSeconds(meleeAttackDelay);
-
-            animator.SetTrigger("MeleeAttack");
+            
+            _slimeAnimator.AsyncPlay(SlimeAnimator.AnimationType.MELEE_ATTACK);
+            yield return new WaitForSeconds(SlimeAnimator.ANIMATION_DELTA_TIME * 2);
+            
             float distance = Vector3.Distance(transform.position, target.position);
             if (distance < MeleeAttackRange) PlayerCharacter.Instance.TakeDamage(damage);
-
-            yield return new WaitForSeconds(animatorDelay);
-            animator.SetTrigger("Idle");
+            
+            yield return new WaitForSeconds(SlimeAnimator.ANIMATION_DELTA_TIME);
+            _slimeAnimator.AsyncPlay(SlimeAnimator.AnimationType.IDLE, true);
+            
             yield return new WaitForSeconds(animatorDelay);
             isMove = true;
         }
@@ -147,15 +159,15 @@ namespace Creature.Minion.Slime
         {
             if (curCoroutine != null) StopCoroutine(curCoroutine);
             isMove = false;
-            animator.SetTrigger("RangedCharge");
-            yield return new WaitForSeconds(rangedAttackDelay);
-
-            animator.SetTrigger("RangedAttack");
+            
+            _slimeAnimator.AsyncPlay(SlimeAnimator.AnimationType.RANGED_ATTACK);
+            yield return new WaitForSeconds(SlimeAnimator.ANIMATION_DELTA_TIME);
+            
             GameObject attack = Instantiate(rangedAttack, transform.position, Quaternion.identity, transform);
             attack.SetActive(true);
-
-            yield return new WaitForSeconds(animatorDelay);
-            animator.SetTrigger("Idle");
+            
+            yield return new WaitForSeconds(SlimeAnimator.ANIMATION_DELTA_TIME);
+            _slimeAnimator.AsyncPlay(SlimeAnimator.AnimationType.IDLE, true);
             yield return new WaitForSeconds(animatorDelay);
             isMove = true;
         }
@@ -164,18 +176,19 @@ namespace Creature.Minion.Slime
         {
             if (curCoroutine != null) StopCoroutine(curCoroutine);
             isMove = false;
-            animator.SetTrigger("MeleeCharge");
+            
+            _slimeAnimator.AsyncPlay(SlimeAnimator.AnimationType.MELEE_ATTACK, true);
 
             Vector3 direction = (target.position - transform.position).normalized;
             float dashDistance = 1.5f;
             Vector3 dashTargetPosition = target.position + direction * dashDistance;
             yield return new WaitForSeconds(dashAttackDelay);
-
-            animator.SetTrigger("DashAttack");
+            
             transform.DOMove(dashTargetPosition, 0.3f).SetEase(Ease.OutQuad);
 
             yield return new WaitForSeconds(animatorDelay);
-            animator.SetTrigger("Idle");
+            
+            _slimeAnimator.AsyncPlay(SlimeAnimator.AnimationType.IDLE, true);
             yield return new WaitForSeconds(animatorDelay);
             isMove = true; 
         }
@@ -200,7 +213,7 @@ namespace Creature.Minion.Slime
             }
             else
             {
-                animator.SetTrigger("Dead");
+                _slimeAnimator.AsyncPlay(SlimeAnimator.AnimationType.DIE);
                 yield return new WaitForSeconds(0.3f);
             }
             Destroy(gameObject);
@@ -213,18 +226,20 @@ namespace Creature.Minion.Slime
                 Vector3 spawnPosition = transform.position + Random.insideUnitSphere;
                 
                 GameObject newSlime = Instantiate(gameObject, spawnPosition, Quaternion.identity);
-                Destroy(newSlime.GetComponent<DivisionSlime>());
+                // Destroy(newSlime.GetComponent<DivisionSlime>());
                 
                 newSlime.transform.localScale = transform.localScale * divideRatio;
                 
                 newSlime.transform.DOMoveY(spawnPosition.y + 1f, 0.3f).SetEase(Ease.OutQuad)
                     .OnComplete(() => newSlime.transform.DOMoveY(spawnPosition.y, 0.3f).SetEase(Ease.InBounce));
 
-                DivisionSlime slimeComponent = newSlime.AddComponent<DivisionSlime>();
+                DivisionSlime slimeComponent = newSlime.GetComponent<DivisionSlime>();
+                
                 slimeComponent.damage *= damageRatio;
                 slimeComponent.MeleeAttackRange *= divideRatio;
                 slimeComponent.RangedAttackRange *= divideRatio;
                 slimeComponent.divisionLevel = divisionLevel + 1;
+                slimeComponent.StartMob();
             }
         }
     }
