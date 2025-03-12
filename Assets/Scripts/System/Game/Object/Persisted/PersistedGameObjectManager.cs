ad93c52f575eccb01c8d67e149fba7b036f19cd2
mandu <jys0972@gmail.com>
2025-03-12 22:16:06 +0900

    feat(PersisttedGameObjectManager.cs) : 저장하고 소환하는 로직 구현

--- Assets/Scripts/System/Game/Object/Persisted/PersistedGameObjectManager.cs
+++ Assets/Scripts/System/Game/Object/Persisted/PersistedGameObjectManager.cs
@@ -1,17 +1,87 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace System.Game.Object.Persisted
 {
     public class PersistedGameObjectManager : SingletonObject<PersistedGameObjectManager>
     {
-        private Dictionary<string, (GameObject prefab, Vector3 position, List<ComponentData> componentDatas)> _persistedGameObjects = new();
+        private Dictionary<string, PersistedGameObjectData> _persistedGameObjects = new();
         
-        public void SavePersistedGameObject(PersistedGameObject persistedGameObject, Vector3 position)
+        private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
         {
-            var datas = persistedGameObject.Save();
-            _persistedGameObjects[datas.uniqueId] = (datas.prefab, position, datas.componentData);
+            ScanPersistedGameObjects();
+            PlacePersistedGameObjects();
+        }
+
+        private void ScanPersistedGameObjects()
+        {
+            PersistedGameObject[] objects = FindObjectsByType<PersistedGameObject>(FindObjectsSortMode.None);
+            foreach (PersistedGameObject persistedGameObject in objects)
+            {
+                if (_persistedGameObjects.ContainsKey(persistedGameObject.UniqueId))
+                { // this GameObject's Data is already saved
+                    ApplyDataToPersistedGameObject(persistedGameObject, SceneManager.GetActiveScene());
+                }
+                else
+                { // this GameObject is not saved yet
+                    SavePersistedGameObject(persistedGameObject);
+                }
+            }
+        }
+        private void PlacePersistedGameObjects()
+        {
+            HashSet<string> existingObjectIds = new HashSet<string>();
+            foreach (var obj in FindObjectsByType<PersistedGameObject>(FindObjectsSortMode.None))
+            {
+                existingObjectIds.Add(obj.UniqueId);
+            }
+
+            foreach (var data in _persistedGameObjects.Values)
+            {
+                if (SceneManager.GetActiveScene().name.Equals(data.sceneName))
+                {
+                    if (existingObjectIds.Contains(data.uniqueId))
+                    {
+                        continue;
+                    }
+
+                    GameObject go = Instantiate(data.prefab, data.position, Quaternion.identity);
+                    PersistedGameObject persistedGameObject = go.GetComponent<PersistedGameObject>();
+                    persistedGameObject.Load(data);
+                }
+            }
         }
         
+        private void Start()
+        {
+            ScanPersistedGameObjects();
+            PlacePersistedGameObjects();
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        private void ApplyDataToPersistedGameObject(PersistedGameObject persistedGameObject, Scene scene)
+        {
+            var data = _persistedGameObjects[persistedGameObject.UniqueId];
+            if (scene.name.Equals(data.sceneName))
+            {
+                persistedGameObject.Load(data);
+            }
+            else
+            {
+                Destroy(persistedGameObject.gameObject);
+            }
+        }
+        
+        private void SavePersistedGameObject(PersistedGameObject persistedGameObject)
+        {
+            var datas = persistedGameObject.Save();
+            _persistedGameObjects[datas.uniqueId] = datas;
+        }
+       
     }
 }
