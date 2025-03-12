b782ec0c734307bc969c5f81711874c75b8b23c1
mandu <jys0972@gmail.com>
2025-03-12 22:15:16 +0900

    refact(PersistedGameObject.cs) : tuple이 아닌 struct로 정보를 다루도록 수정

--- Assets/Scripts/System/Game/Object/Persisted/PersistedGameObject.cs
+++ Assets/Scripts/System/Game/Object/Persisted/PersistedGameObject.cs
@@ -1,37 +1,63 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace System.Game.Object.Persisted
 {
+    [Serializable]
+    public struct PersistedGameObjectData
+    {
+        public string uniqueId;
+        public GameObject prefab;
+        public bool isActive;
+        public string sceneName;
+        public Vector3 position;
+        public List<ComponentData> componentDatas;
+
+        public PersistedGameObjectData(string uniqueId, GameObject prefab, bool isActive, string sceneName, Vector3 position, List<ComponentData> componentDatas)
+        {
+            this.uniqueId = uniqueId; this.prefab = prefab; this.isActive = isActive; this.sceneName = sceneName; this.position = position; this.componentDatas = componentDatas;
+        }
+    }
+    
     public class PersistedGameObject : MonoBehaviour
     {
         [SerializeField] [Tooltip("Reference It's Own Prefab")] private GameObject _prefab;
         [SerializeField] private string _uniqueId;
+        public string UniqueId => _uniqueId;
         
-        private List<ComponentData> _componentData = new();
+        private List<ComponentData> _componentDatas = new();
         
-        public (string uniqueId, GameObject prefab, Vector3 position, List<ComponentData> componentData) Save()
+        public PersistedGameObjectData Save()
         {
             SaveComponents();
-            return (_uniqueId, _prefab, transform.position, _componentData);
+            return new PersistedGameObjectData(
+                _uniqueId, 
+                _prefab, 
+                gameObject.activeSelf, 
+                SceneManager.GetActiveScene().name, 
+                transform.position, 
+                _componentDatas
+                );
         }
         
-        public void Load((string uniqueId, GameObject prefab, Vector3 position, List<ComponentData> componentData) data)
+        public void Load(PersistedGameObjectData data)
         {
             _uniqueId = data.uniqueId;
             _prefab = data.prefab;
             transform.position = data.position;
-            _componentData = data.componentData;
+            _componentDatas = data.componentDatas;
             LoadComponents();
+            gameObject.SetActive(data.isActive);
         }
         
         private void SaveComponents()
         {
-            _componentData.Clear();
+            _componentDatas.Clear();
             var components = GetComponentsInChildren<IPersistedComponent>();
             foreach (IPersistedComponent component in components)
             {
-                _componentData.Add(component.Save());
+                _componentDatas.Add(component.Save());
             }
         }
         
@@ -41,7 +67,7 @@ namespace System.Game.Object.Persisted
             var components = GetComponentsInChildren<IPersistedComponent>();
             foreach (var component in components)
             {
-                component.Load(_componentData[i]);
+                component.Load(_componentDatas[i]);
                 i++;
             }
         }
