14fdee33698076a818c220905dbdf4bac1484f8e
mandu <jys0972@gmail.com>
2025-03-13 17:21:41 +0900

    feat(SwordAura.cs) : sword aura 구현

---
+++ Assets/SwordAura.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Creature.Boss;
+using UnityEngine;
+
+public class SwordAura : BossAttack
+{
+    private static bool toggleYScale = false;
+    protected override void Awake()
+    {
+        damage = 10f;
+        base.Awake();
+    }
+    
+    public static IEnumerator Create(Transform transform, Vector3 direction, GameObject prefab)
+    {
+        GameObject swordAura = Instantiate(prefab, transform.position + direction * 0.5f, Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg), transform);
+        // toggle y scale
+        Vector3 scale = swordAura.transform.localScale;
+        swordAura.transform.localScale = new Vector3(scale.x, scale.y * (toggleYScale ? 1 : -1) , scale.z);
+        toggleYScale = !toggleYScale;
+        return swordAura.GetComponent<SwordAura>().AttackRoutine();
+    }
+
+    private IEnumerator AttackRoutine()
+    {
+        yield return new WaitForSeconds(0.04f * 2);
+        GetComponent<BoxCollider2D>().enabled = true;
+        yield return new WaitForSeconds(0.04f * 2);
+        GetComponent<BoxCollider2D>().enabled = false;
+        yield return new WaitForSeconds(0.04f * 2);
+        Destroy(gameObject);
+    }
+}
