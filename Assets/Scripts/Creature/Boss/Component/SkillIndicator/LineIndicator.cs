6b35633e8b74634687fe70db9b7b40c487530b30
hwanginseop <his001015@gmail.com>
2025-03-20 08:54:39 +0900

    feat(LinelIndicator.cs) :  깜빡이는 효과 추가

--- Assets/Scripts/Creature/Boss/Component/SkillIndicator/LineIndicator.cs
+++ Assets/Scripts/Creature/Boss/Component/SkillIndicator/LineIndicator.cs
@@ -8,26 +8,59 @@ namespace Creature.Boss.Component.SkillIndicator
     public class LineIndicator : IndicatorBase
     {
         [SerializeField] private GameObject linePrefab;
+        [SerializeField] private GameObject indicatorEffecterPrefab;
+        SpriteRenderer lineSpriteRenderer;
+        private float effectSpawnInterval = 0.4f;
+
         public override void Initialize(Vector3 startPosition, Transform target, float range, float duration)
         {
             directionToTarget = (target.position - startPosition).normalized;
             indicator = Instantiate(linePrefab, startPosition, Quaternion.LookRotation(Vector3.forward, directionToTarget)).transform;
+            lineSpriteRenderer = indicator.GetComponent<SpriteRenderer>();
         }
         public override IEnumerator GrowIndicator(Vector3 startPosition, Transform target, float range, float duration)
         {
             float elapsedTime = 0f;
+            float effectElapsedTime = 0f;
             while (elapsedTime < duration)
             {
                 directionToTarget = (target.position - transform.position).normalized;
                 float scaleY = Mathf.Lerp(0, range, elapsedTime / duration);
                 indicator.localScale = new Vector3(0.5f, scaleY, 1);
                 indicator.rotation = Quaternion.LookRotation(Vector3.forward, directionToTarget);
+                effectElapsedTime += Time.deltaTime;
+                if (effectElapsedTime >= effectSpawnInterval)
+                {
+                    StartCoroutine(SpawnEffect(scaleY, range));
+                    effectElapsedTime = 0f;
+                }
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
-
+            Color lineColor = lineSpriteRenderer.color;
+            lineColor.a = 1f;
+            lineSpriteRenderer.color = lineColor;
             yield return new WaitForSeconds(0.2f);
             Destroy(indicator.gameObject);
+        }
+        private IEnumerator SpawnEffect(float scaleY, float range)
+        {
+            GameObject effect = Instantiate(indicatorEffecterPrefab, indicator.position, indicator.rotation);
+            Transform effectTransform = effect.transform;
+            effectTransform.localScale = new Vector3(0.5f, 0, 1);
+            float elapsedTime = 0f;
+            while (elapsedTime < effectSpawnInterval)
+            {
+                if (range <= scaleY + 1)
+                    scaleY = range - 1;
+                float effectScaleY = Mathf.Lerp(0, scaleY + 1, elapsedTime / effectSpawnInterval);
+                effectTransform.localScale = new Vector3(0.5f, effectScaleY, 1);
+                effectTransform.rotation = indicator.rotation;
+                effectTransform.position = indicator.position;
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            Destroy(effect);
         }
     }
 }
