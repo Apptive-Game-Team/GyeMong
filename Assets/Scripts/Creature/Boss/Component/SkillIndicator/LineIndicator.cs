7cc6f2126ddf5a8736d2dd7727b6fbff154de559
hwanginseop <his001015@gmail.com>
2025-03-19 17:05:36 +0900

    refact(SkillIndicator.cs) : 인디케이터 크기 및 생성 위치 조정

--- Assets/Scripts/Creature/Boss/Component/SkillIndicator/LineIndicator.cs
+++ Assets/Scripts/Creature/Boss/Component/SkillIndicator/LineIndicator.cs
@@ -17,7 +17,7 @@ namespace Creature.Boss.Component.SkillIndicator
             while (elapsedTime < duration)
             {
                 float scaleY = Mathf.Lerp(0, range, elapsedTime / duration);
-                indicator.localScale = new Vector3(1, scaleY, 1);
+                indicator.localScale = new Vector3(0.5f, scaleY, 1);
                 indicator.position = startPosition + (direction.normalized * scaleY * 0.5f);
                 elapsedTime += Time.deltaTime;
                 yield return null;
