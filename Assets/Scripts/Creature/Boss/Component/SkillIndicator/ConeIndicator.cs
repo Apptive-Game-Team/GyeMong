98796e44fe2c964d45134d9f814ab766d664e6e1
hwanginseop <his001015@gmail.com>
2025-03-20 05:17:56 +0900

    refact(SkillIndicator.cs) : Cone,Circle 인디케이터 코드 동기화

--- Assets/Scripts/Creature/Boss/Component/SkillIndicator/ConeIndicator.cs
+++ Assets/Scripts/Creature/Boss/Component/SkillIndicator/ConeIndicator.cs
@@ -6,9 +6,10 @@ namespace Creature.Boss.Component.SkillIndicator
 {
     public class ConeIndicator : IndicatorBase
     {
-        [SerializeField] protected GameObject conePrefab;
+        [SerializeField] private GameObject conePrefab;
         public override void Initialize(Vector3 startPosition, Transform target, float range, float duration)
         {
+            directionToTarget = (target.position - startPosition).normalized;
             indicator = Instantiate(conePrefab, startPosition, Quaternion.LookRotation(Vector3.forward, directionToTarget)).transform;
         }
         public override IEnumerator GrowIndicator(Vector3 startPosition, Transform target, float range, float duration)
@@ -16,8 +17,10 @@ namespace Creature.Boss.Component.SkillIndicator
             float elapsedTime = 0f;
             while (elapsedTime < duration)
             {
+                directionToTarget = (target.position - transform.position).normalized;
                 float scaleX = Mathf.Lerp(0, range, elapsedTime / duration);
-                indicator.localScale = new Vector3(scaleX, scaleX, 1);
+                indicator.localScale = new Vector3(scaleX, 0.5f, 1);
+                indicator.rotation = Quaternion.LookRotation(Vector3.forward, directionToTarget);
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
