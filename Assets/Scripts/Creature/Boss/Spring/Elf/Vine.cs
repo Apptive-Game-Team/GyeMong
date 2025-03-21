9a1c3b2e8e87a4a72e8df397715323aa89a3febf
hwanginseop <his001015@gmail.com>
2025-03-22 04:02:35 +0900

    refact(Vine.Prefab) : AttackObjectController 적용

--- Assets/Scripts/Creature/Boss/Spring/Elf/Vine.cs
+++ Assets/Scripts/Creature/Boss/Spring/Elf/Vine.cs
@@ -7,16 +7,6 @@ namespace Creature.Boss.Spring.Elf
     [Obsolete("Use AttackObjectController instead")]
     public class Vine : MonoBehaviour
     {
-        private float damage = 20f;
-        private float attackDelay = 0.5f;
-        private EnemyAttackInfo enemyAttackInfo;
-
-        private void Awake()
-        {
-            enemyAttackInfo = gameObject.AddComponent<EnemyAttackInfo>();
-            enemyAttackInfo.Initialize(damage, null, false, true, true, attackDelay);
-        }
-
         private void OnEnable()
         {
             StartCoroutine(ActivateVine());
