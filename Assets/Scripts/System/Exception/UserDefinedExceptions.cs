d555a3129bc285ac6dcb72190e4702bbe3962fa3
mandu <jys0972@gmail.com>
2025-03-22 17:12:52 +0900

    remove User Defined Exception

--- Assets/Scripts/System/Exception/UserDefinedExceptions.cs
+++
@@ -1,9 +0,0 @@
-using ExceptionBase = System.Exception;
-
-namespace Systems.Exceptions
-{
-    public class NotFoundException : ExceptionBase
-    {
-        public NotFoundException(string message) : base(message) { }
-    }
-}
