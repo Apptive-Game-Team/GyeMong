c6d1950e1fb7559b2b74924830c0ee579bf63293
Monolong <83206119+Monolong@users.noreply.github.com>
2025-03-22 18:43:09 +0900

    Feature/228 (#304)
    * Feat(Rune-Buff-Listner)
    
    - 룬과 버프를 구현된 리스너 인터페이스 형식에 맞춰서 시스템을 만드는 중
    - 미완성됨, 조금 더 시간을 소요해야 할 듯
    - 주요 구현은 BuffManager, BuffHitman쪽의 버프 처리임.
    
    * feat(New Rune)
    
    - 버프히트맨 -> 룬히트맨으로 변경, 룬 효과 적용을 담당. 버프 형식, 코루틴 형식, 값 변경 형식, 동작 변경 형식으로 나눠서 구현 예정.
    
    * Feat(Rune Mechanism)
    
    룬 구조 개편.
    룬 클래스를 추가함.
    룬 클래스는 작동방식에 따라 4가지 하위 클래스로 나뉨.
    값변경, 코루틴 실행, 버프 추가, 플레이어 조작 변경
    패러미터는 RuneContext라는 추상 클래스를 활용해볼까 생각중.
    일단은 여기까지 구현.
    
    * feat(rune comp-stat comp)
    
    - StatComponent 구조 수정. 이름으로 스탯을 찾아 바꿔야 해서(추후에 enum으로 바꿀 생각) Dictionary 사용.
    - 각 Rune의 OnActivate()에 들어갈 하나의 일반적인 패러미터를 사용하려고 RuneContext Class를 사용.
    - 추후 Dictionary가 직렬화되지 않는 문제 수정하려 딕셔너리 직렬화 예정.
    
    * Update Rune.cs
    
    * Refact(StatComp)
    
    - Dictionary 구조는 Editor에서 Serialize 되지 않아 값 설정이나 밸런스 수정 시 불편할 것을 고려해 직렬화가 가능한 List+Class 구조로 변경.
    
    * Fix(Stat Initialize)
    
    - 스탯 초기화 안되는 버그 수정
    - 스탯 데이터 간편하게 수정
    
    * Refactor(Buff, Rune)
    
    - AddBuffRune을 추가하면서 버프 구조를 개선했습니다.
    - 이어서 플레이어 인풋 변화 룬을 구현 할 예정입니다.
    
    * Feat(Event-base System Test)
    
    - Action을 사용한 Event 기반의 시스템을 테스트하려 뭔가를 만들어봤습니다.
    
    * Refact(Runes)
    
    - 다음 작업을 진행하기 전에 급하게 짰던 코드들의 구조와 파일들을 정리했습니다.
    
    * feat(chargeSkillAttack)
    
    - 충전식 검기 공격을 구현해보았습니다.
    - 모듈화를 시키고 싶었으나 ...
    - 버프 데이터를 세분화 했습니다.
    
    * Feat(RuneGoldUI)
    
    - 이벤트를 이용해서 룬 골드 UI를 추가했습니다.
    - GoldManager.OnGoldChanged에 메소드를 추가해서 사용하면 됩니다,
    
    * fix(goldManager)
    
    - 골드 매니저 버그 수정
    
    * feat(shieldRune)
    
    - 돌갑옷 룬 버프 추가
    - 디버깅 툴에 버프 꺼지는 이벤트 추가.
    - 사용하지 않는 코드 정리
    
    * Feature(PlayerEvent)
    
    - 플레이어의 행동을 관찰하는 PlayerEvent 클래스를 만들었습니다.
    - 디버깅할 때 사용하기 편하도록 데미지 수치를 표시해주는 기능을 넣을 예정입니다.
    
    * Feat(Upgrade_Emergency)
    
    - 봄바람 룬의 업그레이드 응급 처치를 구현하려고 시도했습니다.
    - PlainBuffData인 120초짜리 쿨타임 버프를 달고 있으면 회복이 안되고, 그거 없고 체력 30% 이하일때 구현하려고 합니다.
    
    * Feat(ShieldGauge)
    
    - 보호막 게이지를 실 수치와 연동했습니다.
    - 일단 기존 콜-리스너 방식으로 구현했으나, 추후에 Action 방식으로 수정해도 될 것 같습니다.
    
    * fix(runedata)
    
    - 룬 데이터  수정 (몇몇 룬 수정)
    
    * Update PlayerCharacter.cs
    
    - 현진이의 수정사항을 반영했습니다
    
    * Feat(runeUpgrade)
    
    - 룬 업그레이드 적용을 일부 구현하려고 했습니다.

--- Assets/Scripts/Creature/Player/Component/PlayerChangeListenerCaller.cs
+++ Assets/Scripts/Creature/Player/Component/PlayerChangeListenerCaller.cs
@@ -6,14 +6,20 @@ namespace Creature.Player.Component
     public class PlayerChangeListenerCaller
     {
         private class HpCaller : ChangeListenerCaller<IHpChangeListener, float>{}
+        private class ShieldCaller : ChangeListenerCaller<IShieldChangeListener, float>{}
         private class SkillGaugeCaller : ChangeListenerCaller<ISkillGaugeChangeListener, float>{}
 
         private HpCaller _hpCaller = new();
+        private ShieldCaller _shieldCaller = new();
         private SkillGaugeCaller _skillGaugeCaller = new();
         
         public void AddHpChangeListener(IHpChangeListener listener)
         {
             _hpCaller.AddListener(listener);
+        }
+        public void AddShieldChangeListener(IShieldChangeListener listener)
+        {
+            _shieldCaller.AddListener(listener);
         }
         public void AddSkillGaugeChangeListener(ISkillGaugeChangeListener listener)
         {
@@ -23,6 +29,10 @@ namespace Creature.Player.Component
         public void CallHpChangeListeners(float hp)
         {
             _hpCaller.Call(hp);
+        }
+        public void CallShieldChangeListeners(float amount)
+        {
+            _shieldCaller.Call(amount);
         }
         
         public void CallSkillGaugeChangeListeners(float skillGauge)
