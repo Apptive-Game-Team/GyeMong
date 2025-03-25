8cdf96b673eab576af2797e4a94621c15402bec7
191012 <kimhj191012@naver.com>
2025-03-25 14:29:12 +0900

    feat(BreakableObject.cs) : 파괴 가능한 오브젝트에 Material 추가

--- Assets/Scripts/Map/Objects/BreakableObject.cs
+++ Assets/Scripts/Map/Objects/BreakableObject.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
+using System.Collections;
 
 namespace Map.Objects
 {
     public class BreakableObject : MonoBehaviour, IAttackable
     {
+        private const float BLINK_DELAY = 0.15f;
         [SerializeField] protected float hp;
+        private Color? _originalColor = null;
+        private MaterialController _materialController;
+        public MaterialController MaterialController
+        {
+            get
+            {
+                if (_materialController == null)
+                {
+                    _materialController = GetComponent<MaterialController>();
+                }
+
+                return _materialController;
+            }
+        }
+        
         public void OnAttacked(float damage = 0)
         {
             print("BreakableObject Attacked");
             hp -= damage;
+            StartCoroutine(Blink());
             if (hp <= 0)
             {
                 DestroyEvent();
@@ -18,6 +36,23 @@ namespace Map.Objects
         public virtual void DestroyEvent()
         {
             Destroy(gameObject);   
+        }
+
+        private IEnumerator Blink()
+        {
+            MaterialController?.SetMaterial(MaterialController.MaterialType.HIT);
+            MaterialController?.SetFloat(1);
+            if (!_originalColor.HasValue)
+                _originalColor = GetComponent<SpriteRenderer>().color;
+            ;
+            GetComponent<SpriteRenderer>().color = Color.white;
+            yield return new WaitForSeconds(BLINK_DELAY);
+            if (MaterialController?.GetCurrentMaterialType() == MaterialController.MaterialType.HIT)
+            {
+                MaterialController?.SetFloat(0);
+            }
+
+            GetComponent<SpriteRenderer>().color = _originalColor.Value; 
         }
     }
 }
