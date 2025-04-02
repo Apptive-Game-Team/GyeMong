94e8d5793b685f04036a5d78f018b1ecf95b0a52
hwanginseop <163392234+hwanginseop@users.noreply.github.com>
2025-04-02 15:40:40 +0900

    refact(Elf.cs) : 패턴 일부 사운드 교체

--- Assets/Scripts/Creature/Mob/StateMachineMob/Boss/Spring/Elf/Elf.cs
+++ Assets/Scripts/Creature/Mob/StateMachineMob/Boss/Spring/Elf/Elf.cs
@@ -284,7 +284,7 @@ namespace Creature.Mob.Boss.Spring.Elf
                 Elf.Animator.SetBool("attackDelay", false);
                 Elf.Animator.SetBool("isAttack", true);
                 Elf.Animator.SetFloat("attackType", 4);
-                Sound.Play("ENEMY_Whip");
+                Sound.Play("EFFECT_Sword_Swing");
                 int numberOfObjects = 5;
                 float interval = 0.2f;
                 float fixedDistance = 7f;
