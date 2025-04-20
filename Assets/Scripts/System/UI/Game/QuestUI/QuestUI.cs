80b41f52e51d9d5f8a9aa26855774f4d935679fd
Gimlocal <127363458+Gimlocal@users.noreply.github.com>
2025-04-20 20:13:39 +0900

    feat(QuestUI.cs) : 퀘스트 UI 구현

---
+++ Assets/Scripts/System/UI/Game/QuestUI/QuestUI.cs
@@ -0,0 +1,61 @@
+using System.Input;
+using UnityEngine;
+using playerCharacter;
+using Util.QuestSystem.Component;
+using Util.QuestSystem.Quests;
+using TMPro;
+using UnityEngine.UI;
+
+namespace System.UI.Game.QuestUI
+{
+    public class QuestUI : UIWindowToggler<QuestUI>
+    { 
+        [SerializeField] private GameObject questListview;
+        [SerializeField] private GameObject questPrefab;
+        [SerializeField] private TMP_Text questDescriptionText;
+        [SerializeField] private TMP_Text goalDescriptionText;
+    
+        protected override void Awake()
+        {
+            base.Awake();
+            toggleKeyActionCode = ActionCode.Menu;
+            InitializeQuestList();
+        }
+        
+        private void InitializeQuestList()
+        {
+            var questComponent = PlayerCharacter.Instance.GetComponent<QuestComponent>();
+            var questInfos = questComponent.GetQuestInfos();
+
+            foreach (var questInfo in questInfos)
+            {
+                CreateQuestButton(questInfo);
+            }
+        }
+        
+        private void CreateQuestButton(SerializableQuestInfo questInfo)
+        {
+            var buttonObj = Instantiate(questPrefab, questListview.transform);
+            buttonObj.GetComponentInChildren<TMP_Text>().text = questInfo.QuestName;
+
+            buttonObj.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                ShowQuestDetail(questInfo);
+            });
+        }
+        
+        private void ShowQuestDetail(SerializableQuestInfo questInfo)
+        {
+            questDescriptionText.text = questInfo.QuestDescription;
+            
+            var goalsText = "";
+            foreach (var goal in questInfo.Goals)
+            {
+                goalsText += $"- {goal.goalDescription}\n";
+            }
+
+            goalDescriptionText.text = goalsText;
+        }
+    }
+}
+
