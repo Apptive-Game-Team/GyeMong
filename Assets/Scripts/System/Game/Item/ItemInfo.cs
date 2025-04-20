9e8656386a5564647257ea3852105c556036c4a5
hwanginseop <his001015@gmail.com>
2025-04-21 01:02:31 +0900

    feat(ItemInfo.cs) : 아이템 인포 scriptableObj 생성

---
+++ Assets/Scripts/System/Game/Item/ItemInfo.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace System.Game.Item
+{
+    [Serializable]
+    public enum ItemType
+    {
+        IDLE = 0,
+        QUEST = 1,
+    }
+    [CreateAssetMenu(fileName = "ItemInfo", menuName = "ScriptableObject/ItemInfo")]
+    public class ItemInfo : ScriptableObject
+    {
+        [SerializeField] private ItemType itemType;
+        public ItemType Type
+        {
+            get
+            {
+                return itemType;
+            }
+        }
+        [SerializeField] private string itemName;
+        public string ItemName
+        {
+            get
+            {
+                return itemName;
+            }
+        }
+        [SerializeField] private string itemDescription;
+        public string ItemDescription
+        {
+            get
+            {
+                return itemDescription;
+            }
+        }
+        [SerializeField] private Sprite itemImage;
+        public Sprite Image
+        {
+            get
+            {
+                return itemImage;
+            }
+        }
+    }
+}
