a5bf18fec81e9c3a380401ea1e2e3dac105b6c90
Tonomi <163392234+hwanginseop@users.noreply.github.com>
2025-04-28 19:31:09 +0900

    Feat/321 (#329)
    * feat(ItemInfo.cs) : 아이템 인포 scriptableObj 생성
    
    * feat(Inventory) : 인벤토리 윈도우와 테스트 씬 생성
    
    * feat(InventoryCanvas) : 인벤토리 Ui 제작
    
    * feat(InventoryCanvas) : 인벤토리 Ui 제작
    
    * feat(ItemObject.cs) : 아이템 오브젝트 스크립트 생성
    
    * feat(InventorySlot.cs) : 인벤토리 슬롯 생성
    
    * comment(ItemObject.cs) : 주석 달기
    
    * refact(Inventory.ui) : Tmp 폰트 변경
    
    * feat(*.cs) : 인벤토리, 아이템 기본 매서드 구현
    
    * Creat Obj for Test Inventory System
    
    * feat(*.cs) : 인벤토리, 아이템 기본 시스템 구현
    
    * Creat Obj for Test Inventory System
    
    * feat(InventorySlot.cs) : 아이템 클릭 시 깜빡임 적용
    
    * Comment(InventorySlot.cs) :모르는거 적음
    
    * refact(InventoryData.cs) : 싱클톤 정상화
    
    * feat(InventoryData.cs) : 아이템 스택 처리 기능 구현
    
    * Change Variable name

---
+++ Assets/Scripts/System/Game/Inventory/InventoryWindowToggler.cs
@@ -0,0 +1,14 @@
+using System.Input;
+using System.UI;
+
+namespace System.Game.Inventory
+{
+    public class InventoryWindowToggler : UIWindowToggler<InventoryWindowToggler>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            toggleKeyActionCode = ActionCode.RunePage;
+        }
+    }
+}
