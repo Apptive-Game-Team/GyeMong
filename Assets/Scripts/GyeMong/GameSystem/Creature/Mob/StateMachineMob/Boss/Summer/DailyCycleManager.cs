3420e893d88f23ae4d4e3aff291afa52c297acc6
hwanginseop <his001015@gmail.com>
2025-05-22 04:25:04 +0900

    refact(DailyCycleManager.cs) : 시간 별 라이트 시스템 적용

--- Assets/Scripts/GyeMong/GameSystem/Creature/Mob/StateMachineMob/Boss/Summer/DailyCycleManager.cs
+++ Assets/Scripts/GyeMong/GameSystem/Creature/Mob/StateMachineMob/Boss/Summer/DailyCycleManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class DailyCycleManager : MonoBehaviour
 {
@@ -8,41 +9,55 @@ public class DailyCycleManager : MonoBehaviour
 
     public float secondsPerFullDay = 120f;
     public float currentTimePercent = 0f;
+    public Light2D globalLight2D;
 
-    public TimeOfDay currentSection;
     private float timePerSecond;
 
-    void Start()
+    private Color dawnColor = new Color32(145, 145, 145, 255);
+    private Color dayColor = new Color32(255, 255, 255, 255);
+    private Color duskColor = new Color32(145, 145, 145, 255);
+    private Color nightColor = new Color32(30, 30, 30, 255);
+
+    private void Start()
     {
         timePerSecond = 1f / secondsPerFullDay;
-        UpdatePhase();
     }
 
-    void Update()
+    private void Update()
     {
         currentTimePercent += timePerSecond * Time.deltaTime;
-        if (currentTimePercent >= 1f) currentTimePercent = 0f;
+        if (currentTimePercent >= 1f)
+            currentTimePercent -= 1f;
 
-        UpdatePhase();
+        UpdateLighting();
     }
 
-    void UpdatePhase()
+    void UpdateLighting()
     {
-        TimeOfDay newPhase;
+        Color targetColor;
 
-        if (currentTimePercent < 0.25f) newPhase = TimeOfDay.Dawn;
-        else if (currentTimePercent < 0.5f) newPhase = TimeOfDay.Day;
-        else if (currentTimePercent < 0.75f) newPhase = TimeOfDay.Dusk;
-        else newPhase = TimeOfDay.Night;
-
-        if (newPhase != currentSection)
+        if (currentTimePercent < 0.25f)
         {
-            currentSection = newPhase;
-            OnPhaseChanged(currentSection);
+            float t = currentTimePercent / 0.25f;
+            targetColor = Color.Lerp(dawnColor, dayColor, t);
         }
-    }
+        else if (currentTimePercent < 0.5f)
+        {
+            float t = (currentTimePercent - 0.25f) / 0.25f;
+            targetColor = Color.Lerp(dayColor, duskColor, t);
+        }
+        else if (currentTimePercent < 0.75f)
+        {
+            float t = (currentTimePercent - 0.5f) / 0.25f;
+            targetColor = Color.Lerp(duskColor, nightColor, t);
+        }
+        else
+        {
+            float t = (currentTimePercent - 0.75f) / 0.25f;
+            targetColor = Color.Lerp(nightColor, dawnColor, t);
+        }
 
-    void OnPhaseChanged(TimeOfDay phase)
-    {
+        globalLight2D.color = targetColor;
     }
 }
+
