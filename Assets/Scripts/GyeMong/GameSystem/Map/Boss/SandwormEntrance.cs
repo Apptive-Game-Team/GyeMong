5696c89d217217b4dddd9da8bc98a4c66dda78d4
191012 <kimhj191012@naver.com>
2025-05-28 16:36:10 +0900

    feat(SandwormEntrance.cs) : 보스 시작 이벤트 코드로 추가

---
+++ Assets/Scripts/GyeMong/GameSystem/Map/Boss/SandwormEntrance.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using GyeMong.EventSystem.Controller;
+using GyeMong.EventSystem.Event;
+using GyeMong.EventSystem.Event.Boss;
+using GyeMong.EventSystem.Event.Chat;
+using GyeMong.EventSystem.Event.CinematicEvent;
+using GyeMong.EventSystem.Event.Input;
+using GyeMong.GameSystem.Creature.Mob.StateMachineMob.Boss.Sandworm;
+using GyeMong.SoundSystem;
+using UnityEngine;
+using Visual.Camera;
+
+namespace GyeMong.GameSystem.Map.Boss
+{
+    public class SandwormEntrance : BossRoomEntrance
+    {
+        [SerializeField] private Vector3 playerDestination;
+        [SerializeField] private float playerMoveSpeed;
+        [SerializeField] private GameObject wall;
+        [SerializeField] private Vector3 cameraDestination;
+        [SerializeField] private float cameraSpeed;
+        [SerializeField] private float cameraZoomSize;
+        [SerializeField] private float cameraZoomSpeed;
+        [SerializeField] private List<MultiChatMessage> multiMessages;
+        [SerializeField] private float autoSkipTime = 3f;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                StartCoroutine(TriggerEvents());
+            }
+        }
+        
+        private IEnumerator TriggerEvents()
+        {
+            yield return StartCoroutine( (new SetKeyInputEvent(){_isEnable = false}).Execute());
+            yield return StartCoroutine((new MoveCreatureEvent()
+            {
+                creatureType = MoveCreatureEvent.CreatureType.Player,
+                iControllable = null,
+                speed = playerMoveSpeed,
+                target = playerDestination
+            }).Execute());
+            yield return StartCoroutine((new SetActiveObject()
+            {
+                _gameObject = wall,
+                isActive = true
+            }).Execute());
+            yield return StartCoroutine((new OpenChatEvent().Execute()));
+            foreach (MultiChatMessage chat in multiMessages)
+            {
+                yield return EffectManager.Instance.GetChatController().MultipleChat(chat, autoSkipTime);
+            }
+            yield return StartCoroutine((new CloseChatEvent().Execute()));
+            yield return StartCoroutine( (new SetKeyInputEvent(){_isEnable = false}).Execute());
+            yield return StartCoroutine(CameraManager.Instance.CameraMove(cameraDestination, cameraSpeed));
+            boss.GetComponent<Sandworm>().curBGM = Sound.Play("BGM_Summer_Sandworm", true);
+            yield return StartCoroutine(CameraManager.Instance.CameraZoomInOut(cameraZoomSize, cameraZoomSpeed));
+            yield return StartCoroutine((new ShowBossHealthBarEvent() { _boss = boss }).Execute());
+            boss.ChangeState();
+            yield return StartCoroutine( (new SetKeyInputEvent(){_isEnable = true}).Execute());
+        }
+    }
+}
