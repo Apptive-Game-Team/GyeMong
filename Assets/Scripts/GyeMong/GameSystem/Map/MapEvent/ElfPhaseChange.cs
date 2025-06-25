0388e7affe10763460eb28ac6345735c6ddbac89
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-06-26 08:34:45 +0900

    Finish ElfPhaseChange

--- Assets/Scripts/GyeMong/GameSystem/Map/MapEvent/ElfPhaseChange.cs
+++ Assets/Scripts/GyeMong/GameSystem/Map/MapEvent/ElfPhaseChange.cs
@@ -1,15 +1,13 @@
 using GyeMong.EventSystem.Event.Boss;
+using GyeMong.GameSystem.Creature;
 using GyeMong.EventSystem.Event.Chat;
 using GyeMong.EventSystem.Event.CinematicEvent;
 using GyeMong.EventSystem.Event.Input;
-using GyeMong.GameSystem.Creature.Mob.StateMachineMob.Boss;
-using GyeMong.GameSystem.Creature.Mob.StateMachineMob.Minion.Slime.Components;
-using GyeMong.GameSystem.Map.Boss;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static GyeMong.GameSystem.Map.MapEvent.SpringNorthForestEntrance;
 using Visual.Camera;
+using GyeMong.EventSystem.Interface;
 
 namespace GyeMong.GameSystem.Map.MapEvent
 {
@@ -19,6 +17,9 @@ namespace GyeMong.GameSystem.Map.MapEvent
 
         [SerializeField] private MultiChatMessageData chatData;
         [SerializeField] private float autoSkipTime = 3f;
+
+        [SerializeField] private Creature.Mob.StateMachineMob.Boss.Boss boss;
+
         public IEnumerator Trigger()
         {
             return TriggerEvents();
@@ -56,6 +57,17 @@ namespace GyeMong.GameSystem.Map.MapEvent
             animBoolEvent.SetAnimator(someAnimator);
             animBoolEvent.SetParameter("isDown", false);
             yield return animBoolEvent.Execute();
+
+            //7
+            boss.Trigger();
+
+            //8
+            yield return new HideBossHealthBarEvent().Execute();
+
+            //9
+            var showHpBar = new ShowBossHealthBarEvent();
+            showHpBar.SetBoss(boss);
+            yield return showHpBar.Execute();
         }
     }
 }
