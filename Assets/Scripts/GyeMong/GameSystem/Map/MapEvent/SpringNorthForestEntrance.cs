2844a47042a066dac826f75913d46299585291fc
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-06-25 21:55:47 +0900

    Update SpringNorthForest : Debug 추가

--- Assets/Scripts/GyeMong/GameSystem/Map/MapEvent/SpringNorthForestEntrance.cs
+++ Assets/Scripts/GyeMong/GameSystem/Map/MapEvent/SpringNorthForestEntrance.cs
@@ -13,24 +13,32 @@ namespace GyeMong.GameSystem.Map.MapEvent
 {
     public class SpringNorthForestEntrance : MonoBehaviour
     {
+        [Header("Stop Animator")]
         [SerializeField] private Animator targetAnimator;
 
+        [Header("Change Sprite : elf_back")]
         [SerializeField] private SpriteRenderer targetSpriteRenderer;
         [SerializeField] private Sprite newSprite;
 
+        [Header("Move Creature : Player")]
         [SerializeField] private Transform moveTarget;
         [SerializeField] private float moveSpeed = 2f;
 
+        [Header("Camera Move")]
         [SerializeField] private Vector3 cameraDestination;
         [SerializeField] private float cameraSpeed;
 
+        [Header("Boss Room Object")]
         [SerializeField] private GameObject bossRoomObj;
 
+        [Header("Chat Data")]
         [SerializeField] private MultiChatMessageData chatData;
         [SerializeField] private float autoSkipTime = 3f;
 
+        [Header("Start Animator")]
         [SerializeField] private Animator someAnimator;
 
+        [Header("")]
         [SerializeField] private GyeMong.GameSystem.Creature.Mob.StateMachineMob.Boss.Boss boss;
 
 
@@ -54,85 +62,104 @@ namespace GyeMong.GameSystem.Map.MapEvent
         {
             _isTriggered = true;
 
-            //0
+            //0 : Input Key False
             yield return StartCoroutine((new SetKeyInputEvent() { _isEnable = false }).Execute());
+            Debug.Log("0");
 
-            //1
+            //1 : Stop Animator
             var stopAnimEvent = new CustomStopAnimatorEvent();
             stopAnimEvent.SetAnimator(targetAnimator);
-            yield return StartCoroutine(stopAnimEvent.Execute());
+            yield return stopAnimEvent.Execute();
+            Debug.Log("1");
 
-            //2
+            //2 : Change Sprite
             var changeSpriteEvent = new ChangeSpriteEvent();
             changeSpriteEvent.SetSpriteRenderer(targetSpriteRenderer);
             changeSpriteEvent.SetSprite(newSprite);
             yield return changeSpriteEvent.Execute();
+            Debug.Log("2");
 
-            //3
+            //3 : Move Creature
             var moveEvent = new MoveCreatureEvent();
             moveEvent.creatureType = MoveCreatureEvent.CreatureType.Player;
             moveEvent.iControllable = null;
             moveEvent.target = moveTarget.position;
             moveEvent.speed = moveSpeed;
             yield return StartCoroutine(moveEvent.Execute());
+            Debug.Log("3");
 
-            //4
+            //4 : Camera Move
             yield return StartCoroutine(CameraManager.Instance.CameraMove(cameraDestination, cameraSpeed));
+            Debug.Log("4");
 
-            //5
+            //5 : Activative Boss
             var activateBossRoomEvent = new ActivateBossRoomEvent();
             activateBossRoomEvent.SetBossRoomObject(bossRoomObj);
             yield return activateBossRoomEvent.Execute();
+            Debug.Log("5");
 
             //6
             var deactivateEvent = new DeActivateBossRoomEvent();
             deactivateEvent.SetBossRoomObject(bossRoomObj);
             yield return deactivateEvent.Execute();
+            Debug.Log("6");
 
             //7
             yield return StartCoroutine((new OpenChatEvent().Execute()));
+            Debug.Log("7");
 
             //8
             yield return new ShowMessages(chatData, autoSkipTime).Execute();
+            Debug.Log("8");
 
             //9
             changeSpriteEvent.SetSpriteRenderer(targetSpriteRenderer);
             changeSpriteEvent.SetSprite(newSprite);
             yield return changeSpriteEvent.Execute();
+            Debug.Log("9");
 
             //10
             yield return new WaitForSeconds(delayTime);
+            Debug.Log("10");
 
             //11
             yield return new ShowMessages(chatData, autoSkipTime).Execute();
+            Debug.Log("11");
 
             //12
             yield return StartCoroutine((new CloseChatEvent().Execute()));
+            Debug.Log("12");
 
             //13
             yield return activateBossRoomEvent.Execute();
+            Debug.Log("13");
 
             //14
             var startEvent = new StartAnimatorEvent();
             startEvent.SetAnimator(someAnimator);
             yield return startEvent.Execute();
+            Debug.Log("14");
 
             //15
             var showHpEvent = new ShowBossHealthBarEvent();
             showHpEvent.SetBoss(boss);
             yield return showHpEvent.Execute();
+            Debug.Log("15");
 
             //16
             var animParamEvent = new SetAnimatorParameter();
             animParamEvent._creatureType = SetAnimatorParameter.CreatureType.Player;
             animParamEvent.SetParameter("Speed", 2.0f);
             yield return animParamEvent.Execute();
+            Debug.Log("16");
 
             //17
             yield return StartCoroutine((new SetKeyInputEvent() { _isEnable = true }).Execute());
+            Debug.Log("17");
 
             //18
             CameraManager.Instance.CameraFollow(GameObject.FindGameObjectWithTag("Player").transform);
+            Debug.Log("18");
 
             //19
         }
