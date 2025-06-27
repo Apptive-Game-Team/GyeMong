7bf67a14d5c9a3779ba1edb7bb53e17566b345e4
191012 <kimhj191012@naver.com>
2025-06-27 17:30:37 +0900

    feat(SoundController) : Enemy, Friendly 타입 추가

--- Assets/Scripts/GyeMong/UISystem/Option/Controller/SoundController.cs
+++ Assets/Scripts/GyeMong/UISystem/Option/Controller/SoundController.cs
@@ -1,5 +1,6 @@
 using GyeMong.DataSystem;
 using GyeMong.SoundSystem;
+using UnityEngine;
 using UnityEngine.UI;
 using Util;
 
@@ -11,34 +12,39 @@ namespace GyeMong.UISystem.Option.Controller
         public Scrollbar uiVolumeSlider;
         public Scrollbar bgmVolumeSlider;
         public Scrollbar effectVolumeSlider;
+        public Scrollbar friendlyVolumeSlider;
+        public Scrollbar enemyVolumeSlider;
 
-        private void Start()
-        {
-            InitialSetting();
-        }
-
-        private void InitialSetting()
+        public void InitialSetting()
         {
             masterVolumeSlider = transform.Find("MasterVolumeSlider").GetComponent<Scrollbar>();
             uiVolumeSlider = transform.Find("UIVolumeSlider").GetComponent<Scrollbar>();
             bgmVolumeSlider = transform.Find("BgmVolumeSlider").GetComponent<Scrollbar>();
             effectVolumeSlider = transform.Find("EffectVolumeSlider").GetComponent<Scrollbar>();
+            friendlyVolumeSlider = transform.Find("FriendlyVolumeSlider").GetComponent<Scrollbar>();
+            enemyVolumeSlider = transform.Find("EnemyVolumeSlider").GetComponent<Scrollbar>();
 
             SoundData soundData = DataManager.Instance.LoadSection<SoundData>("SoundData");
             masterVolumeSlider.value = soundData.masterVolume;
             uiVolumeSlider.value = soundData.UIVolume;
             bgmVolumeSlider.value = soundData.bgmVolume;
             effectVolumeSlider.value = soundData.sfxVolume;
+            friendlyVolumeSlider.value = soundData.friendlyVolume;
+            enemyVolumeSlider.value = soundData.enemyVolume;
 
             SoundManager.Instance.SetMasterVolume(soundData.masterVolume);
             SoundManager.Instance.SetVolume(SoundType.UI, soundData.UIVolume);
             SoundManager.Instance.SetVolume(SoundType.BGM, soundData.bgmVolume);
             SoundManager.Instance.SetVolume(SoundType.EFFECT, soundData.sfxVolume);
+            SoundManager.Instance.SetVolume(SoundType.FRIENDLY, soundData.friendlyVolume);
+            SoundManager.Instance.SetVolume(SoundType.ENEMY, soundData.enemyVolume);
 
             masterVolumeSlider.onValueChanged.AddListener(UpdateMasterVolume);
             uiVolumeSlider.onValueChanged.AddListener(UpdateUIVolume);
             bgmVolumeSlider.onValueChanged.AddListener(UpdateBgmVolume);
             effectVolumeSlider.onValueChanged.AddListener(UpdateEffectVolume);
+            friendlyVolumeSlider.onValueChanged.AddListener(UpdateFriendlyVolume);
+            enemyVolumeSlider.onValueChanged.AddListener(UpdateEnemyVolume);
         }
 
         private void UpdateMasterVolume(float value)
@@ -60,6 +66,15 @@ namespace GyeMong.UISystem.Option.Controller
         {
             SoundManager.Instance.SetVolume(SoundType.EFFECT, value);
         }
-    
+        
+        private void UpdateFriendlyVolume(float value)
+        {
+            SoundManager.Instance.SetVolume(SoundType.FRIENDLY, value);
+        }
+        
+        private void UpdateEnemyVolume(float value)
+        {
+            SoundManager.Instance.SetVolume(SoundType.ENEMY, value);
+        }
     }
 }
