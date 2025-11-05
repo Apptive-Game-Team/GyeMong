a37b10b7c4fc424b3dbdf20a0e39e0186ae8875e
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-11-06 04:11:54 +0900

    Update WandererEntrance Script : 중간저장

--- Assets/Scripts/GyeMong/GameSystem/Map/MapEvent/WandererEntrance.cs
+++ Assets/Scripts/GyeMong/GameSystem/Map/MapEvent/WandererEntrance.cs
@@ -1,9 +1,14 @@
 using GyeMong.EventSystem;
+using GyeMong.EventSystem.Event;
 using GyeMong.EventSystem.Event.Boss;
+using GyeMong.EventSystem.Event.Chat;
+using GyeMong.EventSystem.Event.CinematicEvent;
 using GyeMong.EventSystem.Event.Input;
 using GyeMong.GameSystem.Creature.Mob.StateMachineMob.Boss;
+using GyeMong.GameSystem.Creature.Mob.StateMachineMob.Boss.Summer.NagaWarrior;
 using GyeMong.GameSystem.Creature.Mob.StateMachineMob.Minion.Wanderer;
 using GyeMong.GameSystem.Map.Boss;
+using GyeMong.SoundSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,13 +17,11 @@ namespace GyeMong.GameSystem.Map.MapEvent
 {
     public class WandererEntrance : MonoBehaviour
     {
-        [Header("Boss Room Entrance")]
-        [SerializeField] private BossRoomEntrance bossRoomEntrance;
-
-        [Header("Wanderer")]
-        [SerializeField] private GyeMong.GameSystem.Creature.Mob.StateMachineMob.Minion.Wanderer.Wanderer wanderer;
-
+        [SerializeField] private Vector3 cameraDestination;
+        [SerializeField] private float cameraSpeed;
+        private float delayTime = 1f;
         private bool _isTriggered = false;
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player") && !_isTriggered)
@@ -30,35 +33,18 @@ namespace GyeMong.GameSystem.Map.MapEvent
         private IEnumerator TriggerEvents()
         {
             _isTriggered = true;
-
             yield return StartCoroutine((new SetKeyInputEvent() { _isEnable = false }).Execute());
 
-            var showHpEvent = new ShowWandererHealthBarEvent();
-            showHpEvent.SetWanderer(wanderer);
-            yield return showHpEvent.Execute();
+            yield return StartCoroutine((new SetKeyInputEvent() { _isEnable = false }).Execute());
+            yield return StartCoroutine(SceneContext.CameraManager.CameraMove(cameraDestination, cameraSpeed));
+            //boss.GetComponent<NagaWarrior>().curBGM = Sound.Play("BGM_Summer_NagaWarrior", true);
 
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(delayTime);
 
+            //yield return StartCoroutine((new ShowBossHealthBarEvent() { _boss = boss }).Execute());
+            yield return StartCoroutine((new CameraFollowPlayer()).Execute());
+            //boss.ChangeState();
             yield return StartCoroutine((new SetKeyInputEvent() { _isEnable = true }).Execute());
-        }
-    }
-
-    public class ShowWandererHealthBarEvent
-    {
-        [SerializeField] private GyeMong.GameSystem.Creature.Mob.StateMachineMob.Minion.Wanderer.Wanderer _wanderer;
-
-        public void SetWanderer(GyeMong.GameSystem.Creature.Mob.StateMachineMob.Minion.Wanderer.Wanderer wanderer)
-        {
-            _wanderer = wanderer;
-        }
-
-        public IEnumerator Execute()
-        {
-            var hpBar = SceneContext.EffectManager.GetHpBarController();
-            hpBar.gameObject.SetActive(true);
-            hpBar.ClearBoss();
-            hpBar.UpdateHp(_wanderer.CurrentHp, _wanderer.MaxHp);
-            yield return null;
         }
     }
 }
