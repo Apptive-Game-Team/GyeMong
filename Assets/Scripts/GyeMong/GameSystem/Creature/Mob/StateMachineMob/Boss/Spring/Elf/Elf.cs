ec0f443807c25cc491a48d6f1de1a3ac06b09d31
Tonomi <his001015@gmail.com>
2025-12-10 15:20:03 +0900

    Update(Elf.cs) : 엘프 체력 상향(10/20 = > 15/25)

--- Assets/Scripts/GyeMong/GameSystem/Creature/Mob/StateMachineMob/Boss/Spring/Elf/Elf.cs
+++ Assets/Scripts/GyeMong/GameSystem/Creature/Mob/StateMachineMob/Boss/Spring/Elf/Elf.cs
@@ -48,8 +48,8 @@ namespace GyeMong.GameSystem.Creature.Mob.StateMachineMob.Boss.Spring.Elf
         {
             maxPhase = 2;
             maxHps.Clear();
-            maxHps.Add(10f);
             maxHps.Add(20f);
+            maxHps.Add(30f);
             currentHp = maxHps[currentPhase];
             damage = 20f;
             speed = 2f;
